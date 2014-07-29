1e13e9bfc634a15650fedb6232c89706b17c1be6
Lilfade <lilfade.lor@gmail.com>
2014-07-29 02:18:59 -0500

    some changes to load plugin settings from the db, still need to setup how to save them though but working ok, nothing is used as of yet though need to setup a array in the config.php file to load the settings from the db to be used yet.

--- models/db_functions.php
+++ models/db_functions.php
@@ -1656,8 +1656,7 @@ function fetchConfigParametersPlugins(){
 
         $db = pdoConnect();
 
-        $query = "SELECT id, name, value
-        FROM ".$db_table_prefix."plugin_configuration";
+        $query = "SELECT id, name, value, `binary`, `variable` FROM ".$db_table_prefix."plugin_configuration";
 
         $stmt = $db->prepare($query);
 
@@ -1667,11 +1666,22 @@ function fetchConfigParametersPlugins(){
         }
 
         while ($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $id = $r['id'];
+            $results[$id] = $r;
+        }
+
+        /*while ($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            //$id = $r['id'];
             $name = $r['name'];
             $value = $r['value'];
+            //$binary = $r['binary'];
+            //$variable = $r['variable'];
+
             $results[$name] = $value;
-        }
+        }*/
         $stmt = null;
+
+        ChromePhp::log($results);
 
         return $results;
 
