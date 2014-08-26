0564b6d6c93a59f2778b21dba343103929192673
Lilfade <lilfade.lor@gmail.com>
2014-08-26 05:14:01 -0500

    made some small changes to nav output to fix some display errors due to the $hooks.

--- account/header.php
+++ account/header.php
@@ -48,6 +48,8 @@ $hooks = array(
 // Special case for root account
 if ($loggedInUser->user_id == $master_account){
 	$hooks['#HEADERMESSAGE#'] = "<span class='navbar-center navbar-brand'>YOU ARE CURRENTLY LOGGED IN AS ROOT USER</span>";
+} else {
+    $hooks['#HEADERMESSAGE#'] = "";
 }
 
 //echo fetchUserMenu($loggedInUser->user_id, $hooks)['value'];
@@ -62,19 +64,16 @@ echo '<!-- Brand and toggle get grouped for better mobile display -->
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="../account/index.php">#WEBSITENAME#</a>
-    #HEADERMESSAGE#
+    <a class="navbar-brand" href="../account/index.php">'.$hooks["#WEBSITENAME#"].'</a>
+    '.$hooks["#HEADERMESSAGE#"].'
 </div>
 
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <!-- Collect the nav links, forms, and other content for toggling -->
-
     <ul class="nav navbar-nav side-nav">';
 foreach ($menu as $r => $v){
     if ($v['menu'] == 'left' AND $v['menu'] != 'left-sub'){
-        echo "
-        <li class='navitem-".$v['class_name']."'><a href='../".$v['page']."'><i class='".$v['icon']."'></i> ".$v['name']."</a></li>
-        ";
+        echo "<li class='navitem-".$v['class_name']."'><a href='../".$v['page']."'><i class='".$v['icon']."'></i> ".$v['name']."</a></li>";
     }
     if ($v['menu'] == 'left-sub' AND $v['parent_id'] == 0){
         echo "<li class='dropdown'>
@@ -87,24 +86,18 @@ foreach ($menu as $r => $v){
         foreach ($subs as $s){
             echo "<li class='navitem-".$s['class_name']."'><a href='../".$s['page']."'><i class='".$s['icon']."'></i> ".$s['name']."</a></li>";
         }
-        echo "</ul>
-            </li>";
+        echo '</ul></li>';
     }
 }
-
 echo '</ul>';
-
 //top nav bar
 echo '<ul class="nav navbar-master navbar-nav navbar-right">';
 foreach ($menu as $r => $v){
     if ($v['menu'] == 'top-main' AND $v['menu'] != 'top-main-sub'){
-        echo "
-        <li class='navitem-".$v['class_name']."'><a href='../".$v['page']."'><i class='".$v['icon']."'></i> ".$v['name']."</a></li>
-        ";
+        echo "<li class='navitem-".$v['class_name']."'><a href='../".$v['page']."'><i class='".$v['icon']."'></i> ".$v['name']."</a></li>";
     }
     if ($v['menu'] == 'top-main-sub' AND $v['parent_id'] == 0){
-        echo "
-            <li class='dropdown'>
+        echo "<li class='dropdown'>
             <a href='#' class='dropdown-toggle' data-toggle='dropdown'><i class='".$v['icon']."'></i> ".$v['name']." <b class='caret'></b></a>
                 <ul class='dropdown-menu'>";
         // Grab submenu items based on parent_id = $v['menu_id']
@@ -114,9 +107,7 @@ foreach ($menu as $r => $v){
         foreach ($subs as $s){
             echo "<li class='navitem-".$s['class_name']."'><a href='../".$s['page']."'><i class='".$s['icon']."'></i> ".$s['name']."</a></li>";
         }
-        echo "</ul>
-            </li>";
+        echo '</ul></li>';
     }
 }
-
 echo '</ul></div>';
