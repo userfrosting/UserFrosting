5afca75c0e20d03c16d50539b66f0691b587e2cc
ssnukala <srinivasnukala@SRINIVASs-MBP.home>
2015-07-04 03:45:41 -0500

    Suggested changes to UF Control Classes to Accomodate Plugins
    1. Moved generate Captcha to base class
    2. Updated themes to get the Site Title dynamically
    3. Has the plugin init code
    Updated the userfrosting-oauth plugin code on the other branch.

--- userfrosting/initialize.php
+++ userfrosting/initialize.php
@@ -268,10 +268,27 @@ $view->parserOptions = array(
 
 // TODO: this is where any plugin initialization scripts should be run
 
+$loader->addPath($app->config('plugins.path'));
+
+
+$app->hook('plugins.register', function () use ($app){
+// pickup plugin files
+    $var_plugins = $app->site->getPlugins();
+//print_r($var_plugins);
+//echo("Line 272 plugins");
+    foreach($var_plugins as $var_plugin)
+    {
+//echo(get_include_path() ."<br>".$app->config('plugins.path')."/".$var_plugin."/config-plugin.php");        
+        require_once($app->config('plugins.path')."/".$var_plugin."/config-plugin.php");
+        
+    }
+});
 
 // Hook for core and plugins to register includes
 $app->applyHook("includes.css.register");
 $app->applyHook("includes.js.register");
+
+$app->applyHook("plugins.register");
 
 if ($db_error){
     // In case the error is because someone is trying to reinstall with new db info while still logged in, log them out
