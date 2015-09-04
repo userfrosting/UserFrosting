f4bccdfa5b511fbf61889c58eeb0adf6ac5f8f10
alexweissman <alex@alexanderweissman.com>
2015-09-04 17:24:41 -0400

    more documentation

--- userfrosting/models/UFDatabase.php
+++ userfrosting/models/UFDatabase.php
@@ -9,6 +9,7 @@ namespace UserFrosting;
  * This class acts as a "registry" of sorts, allowing all data model classes to access information about your database,
  * such as table names and whitelisted columns.  Each table in your model shall be represented by a DatabaseTable object.
  *
+ * @see MySqlDatabase
  */
 abstract class UFDatabase {
 
@@ -22,6 +23,13 @@ abstract class UFDatabase {
      */
     protected static $tables;
     
+    /**
+     * Retrieve a DatabaseTable object based on its handle.
+     *
+     * @param string $id the handle (id) of the table, as it was defined in the call to `setTable`.
+     * @throws Exception there is no table associated with the specified handle.
+     * @return DatabaseTable the DatabaseTable registered to this handle.
+     */
     public static function getTable($id){
         if (isset(static::$tables[$id]))
             return static::$tables[$id];
@@ -29,18 +37,40 @@ abstract class UFDatabase {
             throw new \Exception("There is no table with id '$id'.");
     }
     
+    /**
+     * Register a DatabaseTable object with the Database, assigning it the specified handle.
+     *
+     * @param string $id the handle (id) of the table, which you may choose.
+     * @param DatabaseTable $table the DatabaseTable to associate with this handle.
+     * @return void
+     */    
     public static function setTable($id, $table){
         static::$tables[$id] = $table;
     }
     
+    /**
+     * Set the name for a DatabaseTable that has been registered with the database.
+     *
+     * @param string $id the handle (id) of the table, as it was defined in the call to `setTable`.
+     * @param string $name the new name for the DatabaseTable.
+     * @throws Exception there is no table associated with the specified handle.     
+     * @return void
+     */     
     public static function setTableName($id, $name){
         if (isset(static::$tables[$id])) {
-            $columns = array_slice(func_get_args(), 1);
-            call_user_func_array(static::$tables[$id], $columns);
+            call_user_func_array([static::$tables[$id], "setName"], $name);
         } else
             throw new \Exception("There is no table with id '$id'.");
     }
     
+    /**
+     * Add columns to a DatabaseTable that has been registered with the database.
+     *
+     * @param string $id the handle (id) of the table, as it was defined in the call to `setTable`.
+     * @param string $column,... the new columns to add to the DatabaseTable.
+     * @throws Exception there is no table associated with the specified handle.     
+     * @return void
+     */     
     public static function addTableColumns($id){
         if (isset(static::$tables[$id])) {
             $columns = array_slice(func_get_args(), 1);
