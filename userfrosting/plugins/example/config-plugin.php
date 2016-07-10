7f7dd52e7e4001de55049808a2633117df4b80ee
Louis Charette <charette.louis@gmail.com>
2016-07-10 00:27:29 -0400

    Moved the zerglings sidebar link to the example plugin

--- userfrosting/plugins/example/config-plugin.php
+++ userfrosting/plugins/example/config-plugin.php
@@ -7,7 +7,6 @@ $app->schema->registerTwigHook("nav_topbar", "components/dashboard/task-dropdown
 
 //Setup the Twigg template hook for the topbar
 $app->schema->registerTwigHook("nav_sidebar", "components/dashboard/menus/zerg-sidebar.twig");
-
-
+$app->schema->registerTwigHook("nav_sidebar", "components/dashboard/menus/zerglings-sidebar.twig");
 
 ?>
