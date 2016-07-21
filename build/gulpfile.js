4deec1d968e91169fc0c768181c71fe8b2b8846d
alexweissman <alex@alexanderweissman.com>
2016-07-21 15:42:39 -0400

    update sample gulpfile

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -3,13 +3,13 @@ var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
 
 // The directory where the bundle task should look for the raw assets, as specified in bundle.config.json
-var sourceDirectory = '../app/{core/assets/,sprinkles/*/assets/}';
+var sourceDirectory = '../app/sprinkles/*/assets/';
 //['', 
 // The directory where the bundle task should place compiled assets.  The names of assets in bundle.result.json
 // will be specified relative to this path.
 var destDirectory = '../public/assets/';
 
-gulp.task('build', function() {
+gulp.task('build', ['copy'], function() {
     fb = gulp.src('./bundle.config.json')
         .pipe(plugins.bundleAssets({
             base: sourceDirectory
@@ -19,4 +19,14 @@ gulp.task('build', function() {
         })) 
         .pipe(gulp.dest(destDirectory));
     return fb;
+});
+
+gulp.task('copy', function () {
+    // Copy images from core.  Obviously, we will need some way to properly iterate through the sprinkles directory, and override assets as necessary.
+    gulp.src('../app/sprinkles/core/assets/images/**/*')
+    .pipe(gulp.dest(destDirectory + 'images/'));
+
+    // Copy font-awesome font files from core.  Obviously we will want to find all sprinkles automatically, rather than having to explicitly define them here.
+    gulp.src('../app/sprinkles/core/assets/vendor/font-awesome-4.5.0/fonts/**/*')
+    .pipe(gulp.dest(destDirectory + 'fonts/'));
 });
