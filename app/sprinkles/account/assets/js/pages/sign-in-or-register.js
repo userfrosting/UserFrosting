77e2674cd29b79813ce633d5d70cd0ad14e371d2
alexweissman <alex@alexanderweissman.com>
2016-07-23 17:21:47 -0400

    custom styling for register/sign-in buttons

--- app/sprinkles/account/assets/js/pages/sign-in-or-register.js
+++ app/sprinkles/account/assets/js/pages/sign-in-or-register.js
@@ -47,4 +47,15 @@ $(document).ready(function() {
         // Reload captcha
         //$("#captcha").captcha();
     });
+    
+    $("#sign-in").ufForm({
+        validators: page.validators,
+        msgTarget: $("#userfrosting-alerts")
+    }).on("submitSuccess", function() {
+        // Forward to login page on success
+        window.location.replace(site.uri.public + "/account/login");
+    }).on("submitError", function() {
+        // Reload captcha
+        //$("#captcha").captcha();
+    });    
 });
