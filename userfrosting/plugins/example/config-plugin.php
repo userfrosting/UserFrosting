25187e8629325f34d02307671fc7a633b2508ab6
Louis Charette <charette.louis@gmail.com>
2016-08-07 21:27:30 -0400

    Revert "Moved the zerglings sidebar link to the example plugin"
    This reverts commit 7f7dd52e7e4001de55049808a2633117df4b80ee.

--- userfrosting/plugins/example/config-plugin.php
+++ userfrosting/plugins/example/config-plugin.php
@@ -7,6 +7,7 @@ $app->schema->registerTwigHook("nav_topbar", "components/dashboard/task-dropdown
 
 //Setup the Twigg template hook for the topbar
 $app->schema->registerTwigHook("nav_sidebar", "components/dashboard/menus/zerg-sidebar.twig");
-$app->schema->registerTwigHook("nav_sidebar", "components/dashboard/menus/zerglings-sidebar.twig");
+
+
 
 ?>
