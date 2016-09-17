58c4c65896e4ca1573aac37c78f5e12f0baa76bf
Louis Charette <charette.louis@gmail.com>
2016-09-16 21:11:08 -0400

    Begin french translation

--- app/sprinkles/account/locale/fr_FR/account.php
+++ app/sprinkles/account/locale/fr_FR/account.php
@@ -14,13 +14,49 @@
 {{name}} - Dynamic markers which are replaced at run time by the relevant index.
 */
 
-$lang = array();
+return [
+    //Common stuff shared by all Sprinkles
+    "USERNAME" => "Nom d\'utilisateur",
+    "PASSWORD" => "Mot de passe",
+    "FIRST_NAME" => "Prénom",
+    "LAST_NAME" => "Nom",
+    "EMAIL" => "Courriel",
 
-$lang = array_merge($lang,array(
-	"ACCOUNT" => array(
-		"SINGUP" => "Se connecter",
-		"REGISTER" => "Inscription"
-	)
-));
+    "ACCOUNT" => [
+        "@TRANSLATE" => "Profil",
 
-return $lang;
+        "ACTION_SEND_AGAIN" => "Réenvoyer le courriel d'activation",
+
+        "LOGIN" => "Connexion",
+        "LOGIN_SOCIAL" => "Connexion avec",
+        "LOGOUT" => "Déconnexion",
+
+        "REGISTER" => "S'enregistrer",
+        "REGISTER_ME" => "S'enregistrer",
+
+        "PASSWORD_CONFIRM" => "Confirmer le mot de passe",
+        "PASSWORD_BETWEEN" => "Mot de passe (entre {{min}}-{{max}} caratères)",
+        "PASSWORD_FORGET" => "Mot de passe oublié",
+
+        "SIGNIN" => "Se connecter",
+        "SIGNIN_OR_REGISTER" => "Se connecter ou s'inscrire",
+
+        "REGISTER" => "Créer un profil",
+
+        "REMEMBER_ME" => "Se souvenir de moi",
+        "REMEMBER_ME_ON_COMPUTER" => "Se souvenir de moi sur cet ordinateur (non recommandé pour les ordinateurs publics)",
+
+        "CHOOSE_USERNAME" => "Choisir un nom d'utilisateur unique",
+
+        "TOS" => "termes et conditions",
+        "TOS_AGREEMENT" => "En s'enregistrant sur {{site_title}}, vous acceptez les {{&_LINK}}.",
+
+        "PAGE" => [
+            "LOGIN" => [
+                "TITLE" => "Bienvenue !",
+                "SUBTITLE" => "Créez un profil gratuitement ou connectez-vous avec un profil existant",
+                "DESCRIPTION" => "Se connecter avec son profil ou en créer un nouveau.",
+            ]
+        ]
+    ]
+];
