b92f4141c69883e7ab836539b978c1ca00450668
alexweissman <alex@alexanderweissman.com>
2016-09-28 21:11:16 -0400

    fix AuthExpiredException

--- app/sprinkles/account/src/Authenticate/AuthExpiredException.php
+++ app/sprinkles/account/src/Authenticate/AuthExpiredException.php
@@ -8,14 +8,15 @@
  */
 namespace UserFrosting\Sprinkle\Account\Authenticate;
 
-use UserFrosting\Support\Exception\ForbiddenException;
+use UserFrosting\Support\Exception\HttpException;
 
 /**
  * Expired authentication exception.  Used when the user's session has expired due to an expired rememberMe cookie.
  *
  * @author Alex Weissman (https://alexanderweissman.com)
  */
-class AuthExpiredException extends ForbiddenException
+class AuthExpiredException extends HttpException
 {
     protected $default_message = 'ACCOUNT.SESSION_EXPIRED';
+    protected $http_error_code = 403;
 }
