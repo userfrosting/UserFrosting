085438aa776dab84f96c51193480dd32cc331f1d
alexweissman <alex@alexanderweissman.com>
2016-11-14 20:09:37 -0500

    turn off Windows password masking for now

--- migrations/utilities.php
+++ migrations/utilities.php
@@ -12,6 +12,15 @@
         $password = "";
     
         if (strtoupper(substr($os, 0, 3)) === 'WIN') {
+            /*
+             * Password masking in Windows is difficult.  We tried to implement it, but it's not really working.
+             * Sorry, go get a *nix environment instead if you want password masking on the command line.
+             */
+            
+            // read password
+            $password = rtrim(fgets(STDIN), "\r\n");
+            
+            /*
             // Windows.  Need to use _getch()
             // An alternative solution for Windows would be to create a special C program: https://blog.dsl-platform.com/hiding-input-from-console-in-php/
             
@@ -26,7 +35,7 @@
                     // if it's a newline, break out of the loop, we've got our password
                     break;
                 } elseif ("\x08" == $c) {
-                    /* if it's a backspace, delete the previous char from $password */
+                    // if it's a backspace, delete the previous char from $password
                     $password = substr_replace($password,'',-1,1);
                 } elseif ("\x03" == $c) {
                     // if it's Control-C, clear $password and break out of the loop
@@ -37,6 +46,7 @@
                     $password .= $c;
                 }
             }
+            */
         } else {
             // *nix.  Can use stty
             
