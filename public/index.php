7c35bab1c7ea9ef045c0d07fea01b68a13162e51
Jordan Mele <Jordan Mele>
2017-01-13 19:10:52 +1100

    Matching sprinkle.json missing error output to https://github.com/userfrosting/UserFrosting/blob/dev/app/sprinkles/core/src/Handler/ShutdownHandler.php#L59-L70

--- public/index.php
+++ public/index.php
@@ -28,9 +28,19 @@ $container = new Container;
 $sprinklesFile = file_get_contents('../app/sprinkles/sprinkles.json');
 if ($sprinklesFile === false) {
     ob_clean();
-    $errorOutput = '<div style="padding: 50px; background-color: red; color: white; text-align: center;">Unable to start site. Contact owner.</div>';
-    $errorOutput .= '<div style="padding: 10px; background-color: pink; text-align: center;">Version: UserFrosting 4 Pre-Alpha<br/>Error: Unable to determine Sprinkle load order.</div>';
-    exit($errorOutput);
+    $title = "UserFrosting Application Error";
+    $errorMessage = "Unable to start site. Contact owner.<br/><br/>" .
+        "Version: UserFrosting 4 Pre-Alpha<br/>Error: Unable to determine Sprinkle load order.";
+    $output = sprintf(
+        "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>" .
+        "<title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana," .
+        "sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{" .
+        "display:inline-block;width:65px;}</style></head><body><h1>%s</h1>%s</body></html>",
+        $title,
+        $title,
+        $errorMessage
+    );
+    exit($output);
 }
 $sprinkles = json_decode($sprinklesFile);
 
