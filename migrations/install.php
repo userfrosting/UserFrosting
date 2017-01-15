e0cdb841274702fa89e47027f36d0afb5f8f0026
Louis Charette <charette.louis@gmail.com>
2017-01-14 23:29:37 -0500

    New migration system
    Separated the migration per sprinkle. This allows custom sprinkles to
    use this system to install their own database needs.
    Uses a new `version` table to keep track of the (db) installed version

--- migrations/install.php
+++ migrations/install.php
@@ -6,6 +6,7 @@
     use Dotenv\Dotenv;
     use Dotenv\Exception\InvalidPathException;
     use Illuminate\Database\Capsule\Manager as Capsule;
+    use Illuminate\Database\Schema\Blueprint;
     use Slim\Http\Uri;
     use UserFrosting\Sprinkle\Account\Model\User;
     use UserFrosting\Sprinkle\Account\Util\Password;
@@ -60,6 +61,8 @@
 
     $installTime = Carbon::now();
 
+    $UFVersion = "4.0.0-alpha";
+
     $detectedOS = php_uname('s');
 
     echo PHP_EOL . "Welcome to the UserFrosting installation tool!" . PHP_EOL;
@@ -84,89 +87,180 @@
         }
     }
 
-    echo PHP_EOL . "Creating initial tables:" . PHP_EOL;
+    // Get the installed versions
+    echo PHP_EOL . "Checking for Sprinkle's version table:" . PHP_EOL;
 
-    // Sets up database and default groups, roles, and permissions
-    require_once 'schema.php';
+    if (!$schema->hasTable('version')) {
+        $schema->create('version', function (Blueprint $table) {
+            $table->string('sprinkle', 45)->nullable();
+            $table->string('version', 25)->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->collation = 'utf8_unicode_ci';
+            $table->charset = 'utf8';
+            $table->unique('sprinkle');
+        });
+        Capsule::table('version')->insert([
+            [
+                'sprinkle' => 'core',
+                'version' => $UFVersion,
+                'created_at' => $installTime,
+                'updated_at' => $installTime
+            ]
+        ]);
+
+        echo "Installing UserFrosting $UFVersion for the first time..." . PHP_EOL;
+        echo "Created table 'version'..." . PHP_EOL;
+    } else {
+        echo "Table 'version' found." . PHP_EOL;
+    }
+
+    // Load the sprinkles list
+    echo PHP_EOL . "Migrating Sprinkle's:" . PHP_EOL;
+    $sprinklesFile = file_get_contents('../app/sprinkles/sprinkles.json');
+    if ($sprinklesFile === false) {
+        die(PHP_EOL . "File 'app/sprinkles/sprinkles.json' not found. Please try again." . PHP_EOL);
+    }
+    $sprinkles = json_decode($sprinklesFile);
+
+    // Looping throught every sprinkle and running their migration
+    // N.B.: No migrations in core... yet. Add it manually if migration is added
+    // to core at some point
+    foreach ($sprinkles->base as $sprinkle) {
+
+        echo ">> $sprinkle" . PHP_EOL;
+
+        // Find all available version
+        $migrations = glob("../app/sprinkles/$sprinkle/migrations/*.php");
+
+        if (empty($migrations)) {
+            echo "No migrations found for sprinkle '$sprinkle'..." . PHP_EOL.PHP_EOL;
+
+        } else {
+
+            // Get current installed version
+            $installedVersion = Capsule::table('version')->where('sprinkle', $sprinkle)->first();
+            $installedVersion = ($installedVersion != null) ? $installedVersion->version : 0;
+
+            // Loop migrations files and run the ones we needs
+            foreach ($migrations as $filepath) {
+                $version = basename($filepath, ".php");
+                if (version_compare($installedVersion, $version, "<")) {
+                    require_once $filepath;
+                }
+            }
+
+            if ($installedVersion == 0) {
+                Capsule::table('version')->insert([
+                    [
+                        'sprinkle' => $sprinkle,
+                        'version' => $version,
+                        'created_at' => $installTime,
+                        'updated_at' => $installTime
+                    ]
+                ]);
+
+                echo "Migrated sprinkle '$sprinkle' to $version..." . PHP_EOL.PHP_EOL;
+
+            } else if (version_compare($installedVersion, $version, "<")) {
+                Capsule::table('version')->where('sprinkle', $sprinkle)
+                    ->update(
+                        [
+                            'version' => $version,
+                            'updated_at' => $installTime
+                        ]
+                    );
+
+                echo PHP_EOL."Migrated sprinkle '$sprinkle' from $installedVersion to $version..." . PHP_EOL.PHP_EOL;
+            } else {
+                echo "Sprinkle '$sprinkle' already up-to-date..." . PHP_EOL.PHP_EOL;
+            }
+        }
+    }
 
     // Make sure that there are no users currently in the user table
+    // We setup the root account here so it can be done independent of the version check
     if (User::count() > 0) {
-        die(PHP_EOL . "Table 'users' must be empty to set up the root account.  Please truncate or drop the table and try again." . PHP_EOL);
-    }
 
-    echo PHP_EOL . 'To complete the installation process, you must set up a master (root) account.' . PHP_EOL;
-    echo 'Please answer the following questions to complete this process:' . PHP_EOL;
+        echo PHP_EOL . "Table 'users' is not empty. Skipping root account setup. To set up the root account again, please truncate or drop the table and try again." . PHP_EOL;
 
-    // Username
-    echo PHP_EOL . 'Please choose a username (1-50 characters, no leading or trailing whitespace): ';
-    $user_name = rtrim(fgets(STDIN), "\r\n");
-    while (strlen($user_name) < 1 || strlen($user_name) > 50 || !filter_var($user_name, FILTER_VALIDATE_REGEXP, [
-        'options' => [
-            'regexp' => "/^\S((.*\S)|)$/"
-        ]
-    ])) {
-        echo PHP_EOL . "Invalid username '$user_name', please try again: ";
+    } else {
+
+        echo PHP_EOL . 'To complete the installation process, you must set up a master (root) account.' . PHP_EOL;
+        echo 'Please answer the following questions to complete this process:' . PHP_EOL;
+
+        // Username
+        echo PHP_EOL . 'Please choose a username (1-50 characters, no leading or trailing whitespace): ';
         $user_name = rtrim(fgets(STDIN), "\r\n");
-    }
+        while (strlen($user_name) < 1 || strlen($user_name) > 50 || !filter_var($user_name, FILTER_VALIDATE_REGEXP, [
+            'options' => [
+                'regexp' => "/^\S((.*\S)|)$/"
+            ]
+        ])) {
+            echo PHP_EOL . "Invalid username '$user_name', please try again: ";
+            $user_name = rtrim(fgets(STDIN), "\r\n");
+        }
 
-    // Email
-    echo PHP_EOL . 'Please choose a valid email address (1-254 characters, must be compatible with FILTER_VALIDATE_EMAIL): ';
-    $email = rtrim(fgets(STDIN), "\r\n");
-    while (strlen($email) < 1 || strlen($email) > 254 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo PHP_EOL . "Invalid email '$email', please try again: ";
+        // Email
+        echo PHP_EOL . 'Please choose a valid email address (1-254 characters, must be compatible with FILTER_VALIDATE_EMAIL): ';
         $email = rtrim(fgets(STDIN), "\r\n");
-    }
+        while (strlen($email) < 1 || strlen($email) > 254 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo PHP_EOL . "Invalid email '$email', please try again: ";
+            $email = rtrim(fgets(STDIN), "\r\n");
+        }
 
-    // First name
-    echo PHP_EOL . 'Please enter your first name (1-20 characters): ';
-    $first_name = rtrim(fgets(STDIN), "\r\n");
-    while (strlen($first_name) < 1 || strlen($first_name) > 20) {
-        echo PHP_EOL . "Invalid first name '$first_name', please try again: ";
+        // First name
+        echo PHP_EOL . 'Please enter your first name (1-20 characters): ';
         $first_name = rtrim(fgets(STDIN), "\r\n");
-    }
+        while (strlen($first_name) < 1 || strlen($first_name) > 20) {
+            echo PHP_EOL . "Invalid first name '$first_name', please try again: ";
+            $first_name = rtrim(fgets(STDIN), "\r\n");
+        }
 
-    // Last name
-    echo PHP_EOL . 'Please enter your last name (1-30 characters): ';
-    $last_name = rtrim(fgets(STDIN), "\r\n");
-    while (strlen($last_name) < 1 || strlen($last_name) > 30) {
-        echo PHP_EOL . "Invalid last name '$last_name', please try again: ";
+        // Last name
+        echo PHP_EOL . 'Please enter your last name (1-30 characters): ';
         $last_name = rtrim(fgets(STDIN), "\r\n");
-    }
+        while (strlen($last_name) < 1 || strlen($last_name) > 30) {
+            echo PHP_EOL . "Invalid last name '$last_name', please try again: ";
+            $last_name = rtrim(fgets(STDIN), "\r\n");
+        }
 
-    // Password
-    echo PHP_EOL . 'Please choose a password (12-255 characters): ';
-    $password = readPassword($detectedOS);
-    while (strlen($password) < 12 || strlen($password) > 255) {
-        echo PHP_EOL . 'Invalid password, please try again: ';
-        $password = readPassword($detectedOS);
-    }
-
-    // Confirm password
-    echo PHP_EOL . 'Please re-enter your chosen password: ';
-    $password_confirm = readPassword($detectedOS);
-    while ($password !== $password_confirm) {
-        echo PHP_EOL . 'Passwords do not match, please try again. ';
+        // Password
         echo PHP_EOL . 'Please choose a password (12-255 characters): ';
         $password = readPassword($detectedOS);
         while (strlen($password) < 12 || strlen($password) > 255) {
             echo PHP_EOL . 'Invalid password, please try again: ';
             $password = readPassword($detectedOS);
         }
+
+        // Confirm password
         echo PHP_EOL . 'Please re-enter your chosen password: ';
         $password_confirm = readPassword($detectedOS);
+        while ($password !== $password_confirm) {
+            echo PHP_EOL . 'Passwords do not match, please try again. ';
+            echo PHP_EOL . 'Please choose a password (12-255 characters): ';
+            $password = readPassword($detectedOS);
+            while (strlen($password) < 12 || strlen($password) > 255) {
+                echo PHP_EOL . 'Invalid password, please try again: ';
+                $password = readPassword($detectedOS);
+            }
+            echo PHP_EOL . 'Please re-enter your chosen password: ';
+            $password_confirm = readPassword($detectedOS);
+        }
+
+        // Ok, now we've got the info and we can create the new user.
+
+        $rootUser = new User([
+            "user_name" => $user_name,
+            "email" => $email,
+            "first_name" => $first_name,
+            "last_name" => $last_name,
+            "password" => Password::hash($password)
+        ]);
+
+        $rootUser->save();
     }
-
-    // Ok, now we've got the info and we can create the new user.
-
-    $rootUser = new User([
-        "user_name" => $user_name,
-        "email" => $email,
-        "first_name" => $first_name,
-        "last_name" => $last_name,
-        "password" => Password::hash($password)
-    ]);
-
-    $rootUser->save();
 
     /*
     $uri = new Uri(
@@ -180,4 +274,13 @@
     $uri = trim($uri, '/');
     */
 
-    echo PHP_EOL . "Installation is complete.  Please try signing in." . PHP_EOL;
+    // Migrate the UF version
+    Capsule::table('version')->where('sprinkle', 'core')
+        ->update(
+            [
+                'version' => $UFVersion,
+                'updated_at' => $installTime
+            ]
+        );
+
+    echo PHP_EOL.PHP_EOL."UserFrosting migrated to $UFVersion successfully !".PHP_EOL;
