b0629723bc25cd29246f4a455146481056036348
Louis Charette <charette.louis@gmail.com>
2017-01-17 21:35:46 -0500

    Formatting for easier reading

--- app/sprinkles/account/locale/en_US/messages.php
+++ app/sprinkles/account/locale/en_US/messages.php
@@ -24,30 +24,30 @@ return [
         "INVALID" => "This account does not exist. It may have been deleted.  Please contact us for more information.",
 
         "MASTER_NOT_EXISTS" => "You cannot register an account until the master account has been created!",
-        "MY" => "My Account",
+        "MY"                => "My Account",
 
-        "SESSION_COMPROMISED" => "Your session has been compromised.  You should log out on all devices, then log back in and make sure that your data has not been tampered with.",
-        "SESSION_EXPIRED" => "Your session has expired.  Please sign in again.",
+        "SESSION_COMPROMISED"   => "Your session has been compromised.  You should log out on all devices, then log back in and make sure that your data has not been tampered with.",
+        "SESSION_EXPIRED"       => "Your session has expired.  Please sign in again.",
 
         "SETTINGS" => [
-            "@TRANSLATION" => "Account settings",
-            "DESCRIPTION" => "Update your account settings, including email, name, and password.",
-            "UPDATED" => "Account settings updated"
+            "@TRANSLATION"  => "Account settings",
+            "DESCRIPTION"   => "Update your account settings, including email, name, and password.",
+            "UPDATED"       => "Account settings updated"
         ],
 
         "UNVERIFIED" => "Your account has not yet been verified. Check your emails / spam folder for account activation instructions.",
 
         "VERIFICATION" => [
-            "NEW_LINK_SENT" => "We have emailed a new verification link to {{email}}.  Please check your inbox and spam folders for this email.",
-            "RESEND" => "Resend verification email",
-            "COMPLETE" => "You have successfully verified your account. You can now login.",
-            "TOKEN_NOT_FOUND" => "Verification token does not exist / Account is already verified",
+            "NEW_LINK_SENT"     => "We have emailed a new verification link to {{email}}.  Please check your inbox and spam folders for this email.",
+            "RESEND"            => "Resend verification email",
+            "COMPLETE"          => "You have successfully verified your account. You can now login.",
+            "TOKEN_NOT_FOUND"   => "Verification token does not exist / Account is already verified",
         ]
     ],
 
     "EMAIL" => [
-        "INVALID" => "There is no account for '{{email}}'.",
-        "IN_USE" => "Email '{{email}}' is already in use."
+        "INVALID"   => "There is no account for '{{email}}'.",
+        "IN_USE"    => "Email '{{email}}' is already in use."
     ],
 
     "FIRST_NAME" => "First name",
@@ -61,9 +61,9 @@ return [
     "LOGIN" => [
         "@TRANSLATION" => "Login",
 
-        "ALREADY_COMPLETE" => "You are already logged in!",
-        "SOCIAL" => "Or login with",
-        "REQUIRED" => "Sorry, you must be logged in to access this resource."
+        "ALREADY_COMPLETE"  => "You are already logged in!",
+        "SOCIAL"            => "Or login with",
+        "REQUIRED"          => "Sorry, you must be logged in to access this resource."
     ],
 
     "LOGOUT" => "Logout",
@@ -72,72 +72,75 @@ return [
 
     "PAGE" => [
         "LOGIN" => [
-            "DESCRIPTION" => "Sign in to your {{site_name}} account, or register for a new account.",
-            "SUBTITLE" => "Register for free, or sign in with an existing account.",
-            "TITLE" => "Let's get started!",
+            "DESCRIPTION"   => "Sign in to your {{site_name}} account, or register for a new account.",
+            "SUBTITLE"      => "Register for free, or sign in with an existing account.",
+            "TITLE"         => "Let's get started!",
         ]
     ],
 
     "PASSWORD" => [
         "@TRANSLATION" => "Password",
 
-        "BETWEEN" => "Between {{min}}-{{max}} characters",
-        "CONFIRM" => "Confirm password",
-        "CONFIRM_CURRENT" => "Please confirm your current password",
-        "CONFIRM_NEW" => "Confirm New Password",
-        "CONFIRM_NEW_EXPLAIN" => "Re-enter your new password",
-        "CONFIRM_NEW_HELP" => "Required only if selecting a new password",
-        "CURRENT" => "Current Password",
-        "CURRENT_EXPLAIN" => "You must confirm your current password to make changes",
+        "BETWEEN"   => "Between {{min}}-{{max}} characters",
+
+        "CONFIRM"               => "Confirm password",
+        "CONFIRM_CURRENT"       => "Please confirm your current password",
+        "CONFIRM_NEW"           => "Confirm New Password",
+        "CONFIRM_NEW_EXPLAIN"   => "Re-enter your new password",
+        "CONFIRM_NEW_HELP"      => "Required only if selecting a new password",
+        "CURRENT"               => "Current Password",
+        "CURRENT_EXPLAIN"       => "You must confirm your current password to make changes",
+
         "FORGET" => [
             "@TRANSLATION" => "I forgot my password",
 
-            "COULD_NOT_UPDATE" => "Couldn't update password.",
-            "INVALID" => "This password reset request could not be found, or has expired.  Please try <a href=\"{{url}}\">resubmitting your request<a>.",
-            "REQUEST_CANNED" => "Lost password request cancelled.",
-            "REQUEST_SENT" => "A password reset link has been sent to {{email}}."
+            "COULD_NOT_UPDATE"  => "Couldn't update password.",
+            "INVALID"           => "This password reset request could not be found, or has expired.  Please try <a href=\"{{url}}\">resubmitting your request<a>.",
+            "REQUEST_CANNED"    => "Lost password request cancelled.",
+            "REQUEST_SENT"      => "A password reset link has been sent to {{email}}."
         ],
-        "HASH_FAILED" => "Password hashing failed. Please contact a site administrator.",
-        "INVALID" => "Current password doesn't match the one we have on record",
-        "NEW" => "New Password",
+
+        "HASH_FAILED"       => "Password hashing failed. Please contact a site administrator.",
+        "INVALID"           => "Current password doesn't match the one we have on record",
+        "NEW"               => "New Password",
         "NOTHING_TO_UPDATE" => "You cannot update with the same password",
-        "UPDATED" => "Account password updated"
+        "UPDATED"           => "Account password updated"
     ],
 
-    "REGISTER" => "Register",
-    "REGISTER_ME" => "Sign me up",
+    "REGISTER"      => "Register",
+    "REGISTER_ME"   => "Sign me up",
 
     "REGISTRATION" => [
-        "BROKEN" => "We're sorry, there is a problem with our account registration process.  Please contact us directly for assistance.",
-        "COMPLETE_TYPE1" => "You have successfully registered. You can now sign in.",
-        "COMPLETE_TYPE2" => "You have successfully registered. You will soon receive a verification email containing a link to activate your account.  You will not be able to sign in until you complete this step.",
-        "DISABLED" => "We're sorry, account registration has been disabled.",
-        "LOGOUT" => "I'm sorry, you cannot register for an account while logged in. Please log out first.",
-        "WELCOME" => "Registration is fast and simple."
+        "BROKEN"            => "We're sorry, there is a problem with our account registration process.  Please contact us directly for assistance.",
+        "COMPLETE_TYPE1"    => "You have successfully registered. You can now sign in.",
+        "COMPLETE_TYPE2"    => "You have successfully registered. You will soon receive a verification email containing a link to activate your account.  You will not be able to sign in until you complete this step.",
+        "DISABLED"          => "We're sorry, account registration has been disabled.",
+        "LOGOUT"            => "I'm sorry, you cannot register for an account while logged in. Please log out first.",
+        "WELCOME"           => "Registration is fast and simple."
     ],
 
-    "RATE_LIMIT_EXCEEDED" => "The rate limit for this action has been exceeded.  You must wait another {{delay}} seconds before you will be allowed to make another attempt.",
-    "REMEMBER_ME" => "Remember me!",
-    "REMEMBER_ME_ON_COMPUTER" => "Remember me on this computer (not recommended for public computers)",
+    "RATE_LIMIT_EXCEEDED"       => "The rate limit for this action has been exceeded.  You must wait another {{delay}} seconds before you will be allowed to make another attempt.",
+    "REMEMBER_ME"               => "Remember me!",
+    "REMEMBER_ME_ON_COMPUTER"   => "Remember me on this computer (not recommended for public computers)",
 
-    "SIGNIN" => "Sign in",
-    "SIGNIN_OR_REGISTER" => "Sign in or register",
-    "SIGNUP" => "Sign Up",
+    "SIGNIN"                => "Sign in",
+    "SIGNIN_OR_REGISTER"    => "Sign in or register",
+    "SIGNUP"                => "Sign Up",
 
-    "TOS" => "Terms and Conditions",
+    "TOS"           => "Terms and Conditions",
     "TOS_AGREEMENT" => "By registering an account with {{site_title}}, you accept the <a {{link_attributes}}>terms and conditions</a>.",
 
     "USERNAME" => [
         "@TRANSLATION" => "Username",
 
-        "CHOOSE" => "Choose a unique username",
+        "CHOOSE"  => "Choose a unique username",
         "INVALID" => "Invalid username",
-        "IN_USE" => "Username '{{user_name}}' is already in use."
+        "IN_USE"  => "Username '{{user_name}}' is already in use."
     ],
 
-    "USER_ID_INVALID" => "The requested user id does not exist.",
+    "USER_ID_INVALID"       => "The requested user id does not exist.",
     "USER_OR_EMAIL_INVALID" => "Username or email address is invalid.",
-    "USER_OR_PASS_INVALID" => "Username or password is invalid.",
+    "USER_OR_PASS_INVALID"  => "Username or password is invalid.",
 
     "WELCOME" => "Welcome back, {{first_name}}"
 ];
--- app/sprinkles/account/locale/fr_FR/messages.php
+++ app/sprinkles/account/locale/fr_FR/messages.php
@@ -24,30 +24,30 @@ return [
         "INVALID" => "Ce compte n'existe pas. Il a peut-être été supprimé. Veuillez nous contacter pour plus d'informations.",
 
         "MASTER_NOT_EXISTS" => "Vous ne pouvez pas enregistrer un compte tant que le compte principal n'a pas été créé!",
-        "MY" => "Mon compte",
+        "MY"                => "Mon compte",
 
-        "SESSION_COMPROMISED" => "Votre session a été compromise. Vous devez vous déconnecter de tous les périphériques, puis vous reconnecter et vous assurer que vos données n'ont pas été altérées.",
-        "SESSION_EXPIRED" => "Votre session a expiré. Veuillez vous connecter à nouveau.",
+        "SESSION_COMPROMISED"   => "Votre session a été compromise. Vous devez vous déconnecter de tous les périphériques, puis vous reconnecter et vous assurer que vos données n'ont pas été altérées.",
+        "SESSION_EXPIRED"       => "Votre session a expiré. Veuillez vous connecter à nouveau.",
 
         "SETTINGS" => [
-            "@TRANSLATION" => "Paramètres du compte",
-            "DESCRIPTION" => "Mettez à jour les paramètres de votre compte, y compris votre adresse e-mail, votre nom et votre mot de passe.",
-            "UPDATED" => "Paramètres du compte mis à jour"
+            "@TRANSLATION"  => "Paramètres du compte",
+            "DESCRIPTION"   => "Mettez à jour les paramètres de votre compte, y compris votre adresse e-mail, votre nom et votre mot de passe.",
+            "UPDATED"       => "Paramètres du compte mis à jour"
         ],
 
         "UNVERIFIED" => "Votre compte n'a pas encore été vérifié. Vérifiez vos emails / dossier spam pour les instructions d'activation du compte.",
 
         "VERIFICATION" => [
-            "NEW_LINK_SENT" => "Nous avons envoyé un nouveau lien de vérification à {{email}}. Veuillez vérifier vos dossiers de boîte de réception et de spam pour ce courriel.",
-            "RESEND" => "Renvoyer le courriel de validation",
-            "COMPLETE" => "Votre compte a été validé. Vous pouvez maintenant vous connecter.",
-            "TOKEN_NOT_FOUND" => "Le jeton de vérification n'existe pas / Le compte est déjà vérifié",
+            "NEW_LINK_SENT"     => "Nous avons envoyé un nouveau lien de vérification à {{email}}. Veuillez vérifier vos dossiers de boîte de réception et de spam pour ce courriel.",
+            "RESEND"            => "Renvoyer le courriel de validation",
+            "COMPLETE"          => "Votre compte a été validé. Vous pouvez maintenant vous connecter.",
+            "TOKEN_NOT_FOUND"   => "Le jeton de vérification n'existe pas / Le compte est déjà vérifié",
         ]
     ],
 
     "EMAIL" => [
         "INVALID" => "Il n'y a aucun compte pour '{{email}}'.",
-        "IN_USE" => "Le email '{{email}}' est déjà utilisé."
+        "IN_USE"  => "Le email '{{email}}' est déjà utilisé."
     ],
 
     "FIRST_NAME" => "Prénom",
@@ -61,9 +61,9 @@ return [
     "LOGIN" => [
         "@TRANSLATION" => "Connexion",
 
-        "ALREADY_COMPLETE" => "Vous êtes déjà connecté!",
-        "SOCIAL" => "Ou se connecter avec",
-        "REQUIRED" => "Désolé, vous devez être connecté pour accéder à cette ressource."
+        "ALREADY_COMPLETE"  => "Vous êtes déjà connecté!",
+        "SOCIAL"            => "Ou se connecter avec",
+        "REQUIRED"          => "Désolé, vous devez être connecté pour accéder à cette ressource."
     ],
 
     "LOGOUT" => "Déconnexion",
@@ -72,9 +72,9 @@ return [
 
     "PAGE" => [
         "LOGIN" => [
-            "DESCRIPTION" => "Connectez-vous à votre compte {{site_name}} ou enregistrez-vous pour un nouveau compte.",
-            "SUBTITLE" => "Inscrivez-vous gratuitement ou connectez-vous avec un compte existant.",
-            "TITLE" => "Commençons!",
+            "DESCRIPTION"   => "Connectez-vous à votre compte {{site_name}} ou enregistrez-vous pour un nouveau compte.",
+            "SUBTITLE"      => "Inscrivez-vous gratuitement ou connectez-vous avec un compte existant.",
+            "TITLE"         => "Commençons!",
         ]
     ],
 
@@ -82,62 +82,65 @@ return [
         "@TRANSLATION" => "Mot de passe",
 
         "BETWEEN" => "Entre {{min}} et {{max}} charactères",
-        "CONFIRM" => "Confirmer le mot de passe",
-        "CONFIRM_CURRENT" => "Veuillez confirmer votre mot de passe actuel",
-        "CONFIRM_NEW" => "Confirmer le nouveau mot de passe",
-        "CONFIRM_NEW_EXPLAIN" => "Confirmer le mot de passe",
-        "CONFIRM_NEW_HELP" => "Obligatoire uniquement si vous sélectionnez un nouveau mot de passe",
-        "CURRENT" => "Mot de passe actuel",
-        "CURRENT_EXPLAIN" => "Vous devez confirmer votre mot de passe actuel pour apporter des modifications",
+
+        "CONFIRM"               => "Confirmer le mot de passe",
+        "CONFIRM_CURRENT"       => "Veuillez confirmer votre mot de passe actuel",
+        "CONFIRM_NEW"           => "Confirmer le nouveau mot de passe",
+        "CONFIRM_NEW_EXPLAIN"   => "Confirmer le mot de passe",
+        "CONFIRM_NEW_HELP"      => "Obligatoire uniquement si vous sélectionnez un nouveau mot de passe",
+        "CURRENT"               => "Mot de passe actuel",
+        "CURRENT_EXPLAIN"       => "Vous devez confirmer votre mot de passe actuel pour apporter des modifications",
+
         "FORGET" => [
             "@TRANSLATION" => "J'ai oublié mon mot de passe",
 
-            "COULD_NOT_UPDATE" => "Impossible de mettre à jour le mot de passe.",
-            "INVALID" => "Cette requête de réinitialisation de mot de passe n'a pas pu être trouvée ou a expiré. Veuillez réessayer <a href=\"{{url}}\"> de soumettre votre demande <a>.",
-            "REQUEST_CANNED" => "Demande de mot de passe perdu annulée.",
-            "REQUEST_SENT" => "Un lien de réinitialisation du mot de passe a été envoyé à {{email}}."
+            "COULD_NOT_UPDATE"  => "Impossible de mettre à jour le mot de passe.",
+            "INVALID"           => "Cette requête de réinitialisation de mot de passe n'a pas pu être trouvée ou a expiré. Veuillez réessayer <a href=\"{{url}}\"> de soumettre votre demande <a>.",
+            "REQUEST_CANNED"    => "Demande de mot de passe perdu annulée.",
+            "REQUEST_SENT"      => "Un lien de réinitialisation du mot de passe a été envoyé à {{email}}."
         ],
-        "HASH_FAILED" => "Le hachage du mot de passe a échoué. Veuillez contacter un administrateur de site.",
-        "INVALID" => "Le mot de passe actuel ne correspond pas à celui que nous avons au dossier",
+
+        "HASH_FAILED"       => "Le hachage du mot de passe a échoué. Veuillez contacter un administrateur de site.",
+        "INVALID"           => "Le mot de passe actuel ne correspond pas à celui que nous avons au dossier",
         "NOTHING_TO_UPDATE" => "Vous ne pouvez pas mettre à jour avec le même mot de passe",
-        "NEW" => "Nouveau mot de passe",
-        "UPDATED" => "Mot de passe du compte mis à jour"
+        "NEW"               => "Nouveau mot de passe",
+        "UPDATED"           => "Mot de passe du compte mis à jour"
     ],
 
-    "REGISTER" => "S'inscrire",
-    "REGISTER_ME" => "S'inscrire",
+    "REGISTER"      => "S'inscrire",
+    "REGISTER_ME"   => "S'inscrire",
 
     "REGISTRATION" => [
-        "BROKEN" => "Nous sommes désolés, il ya un problème avec notre processus d'enregistrement de compte. Veuillez nous contacter directement pour obtenir de l'aide.",
+        "BROKEN"         => "Nous sommes désolés, il ya un problème avec notre processus d'enregistrement de compte. Veuillez nous contacter directement pour obtenir de l'aide.",
         "COMPLETE_TYPE1" => "Vous êtes inscrit avec succès. Vous pouvez maintenant vous connecter.",
         "COMPLETE_TYPE2" => "Vous êtes inscrit avec succès. Vous recevrez bientôt un e-mail de validation contenant un lien pour activer votre compte. Vous ne pourrez pas vous connecter avant d'avoir terminé cette étape.",
-        "DISABLED" => "Désolé, l'enregistrement de compte a été désactivé.",
-        "LOGOUT" => "Désolé, vous ne pouvez pas vous inscrire tout en étant connecté. Veuillez vous déconnecter en premier.",
-        "WELCOME" => "L'inscription est rapide et simple."
+        "DISABLED"       => "Désolé, l'enregistrement de compte a été désactivé.",
+        "LOGOUT"         => "Désolé, vous ne pouvez pas vous inscrire tout en étant connecté. Veuillez vous déconnecter en premier.",
+        "WELCOME"        => "L'inscription est rapide et simple."
     ],
 
-    "RATE_LIMIT_EXCEEDED" => "La limite tarifaire pour cette action a été dépassée. Vous devez attendre un autre {{délai}} secondes avant de pouvoir effectuer une autre tentative.",
-    "REMEMBER_ME" => "Se souvenir de moi!",
-    "REMEMBER_ME_ON_COMPUTER" => "Se souvenir de moi sur cet ordinateur (non recommandé pour les ordinateurs publics)",
+    "RATE_LIMIT_EXCEEDED"       => "La limite tarifaire pour cette action a été dépassée. Vous devez attendre un autre {{délai}} secondes avant de pouvoir effectuer une autre tentative.",
+    "REMEMBER_ME"               => "Se souvenir de moi!",
+    "REMEMBER_ME_ON_COMPUTER"   => "Se souvenir de moi sur cet ordinateur (non recommandé pour les ordinateurs publics)",
 
-    "SIGNIN" => "Se connecter",
+    "SIGNIN"             => "Se connecter",
     "SIGNIN_OR_REGISTER" => "Se connecter ou s'inscrire",
-    "SIGNUP" => "S'inscrire",
+    "SIGNUP"             => "S'inscrire",
 
-    "TOS" => "Termes et conditions",
+    "TOS"           => "Termes et conditions",
     "TOS_AGREEMENT" => "En créant un compte avec {{site_title}}, vous acceptez les <a {{link_attributes}}>termes et conditions</a>.",
 
     "USERNAME" => [
         "@TRANSLATION" => "Nom d'utilisateur",
 
-        "CHOOSE" => "Choisissez un nom d'utilisateur unique",
+        "CHOOSE"  => "Choisissez un nom d'utilisateur unique",
         "INVALID" => "Nom d'utilisateur invalide",
-        "IN_USE" => "Le nom d'utilisateur '{{username}}' est déjà utilisé."
+        "IN_USE"  => "Le nom d'utilisateur '{{username}}' est déjà utilisé."
     ],
 
-    "USER_ID_INVALID" => "L'identifiant d'utilisateur demandé n'existe pas.",
+    "USER_ID_INVALID"       => "L'identifiant d'utilisateur demandé n'existe pas.",
     "USER_OR_EMAIL_INVALID" => "Nom d'utilisateur ou adresse e-mail non valide.",
-    "USER_OR_PASS_INVALID" => "Nom d'utilisateur ou mot de passe incorrect.",
+    "USER_OR_PASS_INVALID"  => "Nom d'utilisateur ou mot de passe incorrect.",
 
     "WELCOME" => "Bienvenue {{first_name}}"
 ];
