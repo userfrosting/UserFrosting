969dad16030ed25743f32dbf8f659ccfc20b48ae
X-Anonymous-Y <kamilneumann85@gmail.com>
2017-01-23 15:09:25 +0100

    German translation added

---
+++ app/sprinkles/core/locale/de_DE/validate.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * de_DE
+ *
+ * German generic message translations for the core sprinkle.
+ *
+ * @package UserFrosting
+ * @link http://www.userfrosting.com/components/#i18n
+ * @author @X-Anonymous-Y
+ */
+
+return [
+	"VALIDATE" => [
+		"ARRAY" 					    => "Die Werte für '{{label}}' müssen in einem Feld liegen.",
+		"BOOLEAN" 					  => "Der Wert für '{{label}}' muss entweder '0' oder '1' sein.",
+		"INTEGER" 					  => "Der Wert für '{{label}}' muss eine ganze Zahl sein.",
+		"INVALID_EMAIL" 			=> "Ungültige E-Mail-Adresse.",
+		"LENGTH_RANGE" 				=> "{{label}} muss zwischen {{min}} und {{max}} Zeichen lang sein.",
+		"NO_LEAD_WS" 				  => "Der Wert für '{{label}}' kann nicht mit Leerzeichen, Tabulatoren oder anderen Leerzeichen beginnen.",
+		"NO_TRAIL_WS" 				=> "Der Wert für '{{label}}' kann nicht mit Leerzeichen, Tabulatoren oder anderen Leerzeichen enden.",
+		"REQUIRED" 					  => "Bitte geben Sie einen Wert für '{{label}}' an."
+	]
+];
