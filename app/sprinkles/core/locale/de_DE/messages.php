f444f863e1ac76d3253edd49330b557370264e74
X-Anonymous-Y <kamilneumann85@gmail.com>
2017-01-23 15:08:39 +0100

    German translation added

---
+++ app/sprinkles/core/locale/de_DE/messages.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * de_DE
+ *
+ * German message token translations for the core sprinkle.
+ *
+ * @package UserFrosting
+ * @link http://www.userfrosting.com/components/#i18n
+ * @author @X-Anonymous-Y
+ */
+
+return [
+	"@PLURAL_RULE" => 1,
+
+	"ABOUT" 						    => "Über",
+
+	"CAPTCHA" => [
+		"@TRANSLATION" 				=> "Sicherheitscode",
+		"FAIL" 						    => "Sie haben den Sicherheitscode nicht korrekt eingegeben.",
+		"SPECIFY" 					  => "Geben Sie den Sicherheitscode ein",
+		"VERIFY" 					    => "Überprüfen Sie den Sicherheitscode"
+	],
+
+	"CSRF_MISSING" 					=> "Fehlender CSRF-Token. Versuchen, die Seite zu aktualisieren und erneut zu senden?",
+
+	"DB_INVALID" 					  => "Keine Verbindung zur Datenbank möglich. Wenn Sie ein Administrator sind, überprüfen Sie bitte Ihr Fehlerprotokoll.",
+	"DESCRIPTION"   				=> "Beschreibung",
+	"DOWNLOAD" => [
+		"@TRANSLATION" 				=> "Herunterladen",
+		"CSV" 						    => "CSV herunterladen"
+	],
+
+	"EMAIL" => [
+		"@TRANSLATION" 				=> "E-Mail",
+		"YOUR" 						    => "Ihre E-Mail-Adresse"
+	],
+
+	"HOME"  						    => "Startseite",
+
+	"LEGAL" 						    => "Rechtsgrundsatz",
+
+	"LOCALE" 						    => "Gebietsschema",
+
+	"MAIL_ERROR" 					  => "Schwerwiegender Fehler beim Mailversand, wenden Sie sich an Ihren Serveradministrator. Wenn Sie der Administrator sind, überprüfen Sie bitte das UF-Mail-Protokoll.",
+
+	"NAME"  						    => "Name",
+	"NAVIGATION" 					  => "Navigation",
+
+	"PAGINATION" => [
+		"GOTO" 						    => "Gehe zu Seite",
+		"SHOW" 						    => "Anzeigen"
+	],
+	"PRIVACY" 						  => "Datenschutz-Bestimmungen",
+
+	"SLUG" 							    => "Schnecke",
+	"SLUG_CONDITION" 				=> "Schnecke/Bedingungen",
+	"STATUS" 						    => "Status",
+
+	"UNKNOWN" 						  => "Unbekannt",
+
+	// Actions words
+	"ACTIONS" 						  => "Aktionen",
+	"ACTIVATE" 						  => "Aktivieren",
+	"ACTIVE" 						    => "Aktiv",
+	"ADD" 							    => "Hinzufügen",
+	"CANCEL" 						    => "Stornieren",
+	"CONFIRM" 						  => "Bestätigen",
+	"CREATE" 						    => "Erstellen",
+	"DELETE" 						    => "Löschen",
+	"DELETE_CONFIRM" 				=> "Möchten Sie diese wirklich löschen?",
+	"DELETE_CONFIRM_YES"	 	=> "Ja, löschen",
+	"DELETE_CONFIRM_NAMED" 	=> "Möchten Sie {{name}} wirklich löschen?",
+	"DELETE_CONFIRM_YES_NAMED" 	=> "Ja, {{name}} löschen",
+	"DELETE_CANNOT_UNDONE" 	=> "Diese Aktion kann nicht rückgängig gemacht werden.",
+	"DELETE_NAMED" 					=> "{{name}} löschen",
+	"DENY" 							    => "Verweigern",
+	"DISABLE" 						  => "Deaktivieren",
+	"DISABLED" 						  => "Deaktiviert",
+	"EDIT" 							    => "Bearbeiten",
+	"ENABLE" 						    => "Aktivieren",
+	"ENABLED" 						  => "Aktiviert",
+	"OVERWRITE"	 					  => "Überschreiben",
+	"RESET" 						    => "Zurücksetzen",
+	"SAVE" 							    => "Speichern",
+	"SEARCH" 						    => "Suchen",
+	"SORT" 							    => "Sortieren",
+	"SUBMIT" 						    => "Einreichen",
+	"PRINT" 						    => "Drucken",
+	"REMOVE" 						    => "Entfernen",
+	"UNACTIVATED" 					=> "Unaktiviert",
+	"UPDATE" 						    => "Aktualisieren",
+	"YES" 							    => "Ja",
+	"NO" 							      => "Nein",
+	"OPTIONAL" 						  => "Optional",
+
+	// Misc.
+	"BUILT_WITH_UF"         => "Errichtet mit <a href=\"http://www.userfrosting.com\">UserFrosting</a>",
+	"ADMINLTE_THEME_BY"     => "Theme von <strong><a href=\"http://almsaeedstudio.com\">Almsaeed Studio</a>.</strong> Alle Rechte vorbehalten"
+];
