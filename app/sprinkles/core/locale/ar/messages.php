42c67ff61d0e666a419bd645ec9fd7f24ac5262f
abdullahseba <abdullah.seba@outlook.com>
2017-02-09 20:41:57 +0000

    Add files via upload

---
+++ app/sprinkles/core/locale/ar/messages.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ *  ar
+ *
+ * Modern Spoken Arabic message token translations for the core sprinkle
+ *
+ * @package UserFrosting
+ * @link http://wwwuserfrostingcom/components/#i18n
+ * @author Alexander Weissman and Abdullah Seba
+ *
+ */
+
+return [
+    "@PLURAL_RULE" => 1,
+
+    "ABOUT" => "عن",
+
+	"CAPTCHA" => [
+	    "@TRANSLATION" => "كلمة التحقق",
+        "FAIL" => "لم تقم بإدخال رمز كلمة التحقق بشكل صحيح",
+        "SPECIFY" => "أدخل كلمة التحقق",
+        "VERIFY" => "التحقق من كلمة التحقق"
+    ],
+
+    "CSRF_MISSING" => "رمز CSRF غير موجود حاول تحديث الصفحة ومن ثم إرساله مرة أخرى",
+
+    "DB_INVALID" => "لا يمكن الاتصال بقاعدة البيانات إذا كنت مسؤولا، يرجى مراجعة سجل خطأ",
+    "DESCRIPTION"   => "وصف",
+    "DOWNLOAD" => [
+        "@TRANSLATION" => "تحميل",
+        "CSV" => "تحميل CSV"
+    ],
+
+    "EMAIL" => [
+        "@TRANSLATION" => "البريد الإلكتروني",
+        "YOUR" => "عنوان بريدك الإلكتروني"
+    ],
+
+    "HOME"  => "الصفحة الرئيسية",
+
+    "LEGAL" => "السياسة القانونية",
+
+    "LOCALE" => "اللغه",
+
+    "MAIL_ERROR" => "خطأ فادح في محاولة البريد الإلكتروني، اتصل بمسؤول المقع إذا كنت المشرف، يرجى التحقق من التسجل البريد الإلكتروني UF",
+
+    "NAME"  => "اسم",
+    "NAVIGATION" => "التنقل",
+
+    "PAGINATION" => [
+        "GOTO" => "انتقال إلى الصفحة",
+        "SHOW" => "تبين"
+    ],
+    "PRIVACY" => "سياسة الخصوصية",
+
+    "SLUG" => "Slug",
+    "SLUG_CONDITION" => "Slug/الظروف",
+    "STATUS" => "الحالة",
+
+    "UNKNOWN" => "غير معروف",
+
+    // Actions words
+    "ACTIONS" => "الأفعال",
+    "ACTIVATE" => "تفعيل",
+    "ACTIVE" => "نشيط",
+    "ADD" => "إضافة",
+    "CANCEL" => "إلغاء",
+    "CONFIRM" => "تؤكد",
+    "CREATE" => "أنتج",
+    "DELETE" => "حذف",
+    "DELETE_CONFIRM" => "هل أنت متأكد أنك تريد حذف هذا",
+    "DELETE_CONFIRM_YES" => "نعم، احذف",
+    "DELETE_CONFIRM_NAMED" => "هل أنت متأكد أنك تريد حذف {{name}}?",
+    "DELETE_CONFIRM_YES_NAMED" => "نعم، احذف {{name}}",
+    "DELETE_CANNOT_UNDONE" => "لا يمكن التراجع عن هذا الإجراء",
+    "DELETE_NAMED" => "احذف {{name}}",
+    "DENY" => "رفض",
+    "DISABLE" => "تعطيل",
+    "DISABLED" => "معطل",
+    "EDIT" => "تصحيح",
+    "ENABLE" => "تمكين",
+    "ENABLED" => "مكين",
+    "OVERWRITE" => "كتب فوق الكتابة",
+    "RESET" => "إعادة تعيين",
+    "SAVE" => "احفظ",
+    "SEARCH" => "ابحث",
+    "SORT" => "فرز",
+    "SUBMIT" => "ارسل",
+    "PRINT" => "اطباعة",
+    "REMOVE" => "إزالة",
+    "UNACTIVATED" => "إبطال",
+    "UPDATE" => "تحديث",
+    "YES" => "نعم",
+    "NO" => "لا",
+    "OPTIONAL" => "اختياري",
+
+    // Misc
+    "BUILT_WITH_UF" => "بنيت مع <a href=\"http://wwwuserfrostingcom\">UserFrosting</a>",
+    "ADMINLTE_THEME_BY" => "فكرة رئيسية <strong><a href=\"http://almsaeedstudiocom\">Almsaeed Studio</a></strong> كل الحقوق محفوظة"
+];
