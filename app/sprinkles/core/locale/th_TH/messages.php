bb99937621914cbdeeb38593b99f969d4bc926a0
popiazaza <karuhut929@gmail.com>
2017-02-17 07:29:15 +0700

    Add Thai translation files.

---
+++ app/sprinkles/core/locale/th_TH/messages.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * th_TH
+ *
+ * Thai message token translations for the core sprinkle.
+ *
+ * @package UserFrosting
+ * @link http://www.userfrosting.com/components/#i18n
+ * @author Karuhut Komol
+ *
+ */
+
+return [
+    "@PLURAL_RULE" => 1,
+
+    "ABOUT" => "เกี่ยวกับ",
+
+	"CAPTCHA" => [
+	    "@TRANSLATION" => "รหัสยืนยัน",
+        "FAIL" => "คุณยังกรอกรหัสยืนยันไม่ถูกต้อง",
+        "SPECIFY" => "กรอกรหัสยืนยัน",
+        "VERIFY" => "ตรวจสอบรหัสยืนยัน"
+    ],
+
+    "CSRF_MISSING" => "ไม่พบโทเคน CSRF กรุณารีเฟรชแล้วส่งข้อมูลใหม่",
+
+    "DB_INVALID" => "ไม่สามารถเชื่อมต่อกับฐานข้อมูลได้ หากคุณเป็นผู้ดูแลระบบ กรุณาตรวจสอบบันทึกข้อผิดพลาด",
+    "DESCRIPTION"   => "รายละเอียด",
+    "DOWNLOAD" => [
+        "@TRANSLATION" => "ดาวน์โหลด",
+        "CSV" => "ดาวน์โหลด CSV"
+    ],
+
+    "EMAIL" => [
+        "@TRANSLATION" => "อีเมล",
+        "YOUR" => "ที่อยู่อีเมลของคุณ"
+    ],
+
+    "HOME"  => "หน้าแรก",
+
+    "LEGAL" => "นโยบายทางกฎหมาย",
+
+    "LOCALE" => "ภาษา",
+
+    "MAIL_ERROR" => "เกิดข้อผิดพลาดร้ายแรงระหว่างการพยายามส่งอีเมล กรุณาติดต่อผู้ดูแลระบบของเซิฟเวอร์นี้ หากคุณเป็นผู้ดูแล กรุณาตรวจสอบบันทึกอีเมลของ UF",
+
+    "NAME"  => "ชื่อ",
+    "NAVIGATION" => "เมนูนำทาง",
+
+    "PAGINATION" => [
+        "GOTO" => "ข้ามไปยังหน้า",
+        "SHOW" => "แสดง"
+    ],
+    "PRIVACY" => "นโยบายความเป็นส่วนตัว",
+
+    "SLUG" => "ข้อกำหนด",
+    "SLUG_CONDITION" => "ข้อกำหนด/เงื่อนไข",
+    "STATUS" => "สถานะ",
+
+    "UNKNOWN" => "ไม่ทราบ",
+
+    // Actions words
+    "ACTIONS" => "การดำเนินการ",
+    "ACTIVATE" => "เปิดใช้งาน",
+    "ACTIVE" => "เปิดใช้งานอยู่",
+    "ADD" => "เพิ่ม",
+    "CANCEL" => "ยกเลิก",
+    "CONFIRM" => "ยืนยัน",
+    "CREATE" => "สร้าง",
+    "DELETE" => "ลบ",
+    "DELETE_CONFIRM" => "คุณต้องการที่จะลบใช่หรือไม่?",
+    "DELETE_CONFIRM_YES" => "ใช่ ลบเลย",
+    "DELETE_CONFIRM_NAMED" => "คุณต้องการที่จะลบ {{name}} ใช่หรือไม่?",
+    "DELETE_CONFIRM_YES_NAMED" => "ใช่ ลบ {{name}} เลย",
+    "DELETE_CANNOT_UNDONE" => "การดำเนินการนี้ไม่สามารถยกเลิกได้",
+    "DELETE_NAMED" => "ลบ {{name}}",
+    "DENY" => "ปฏิเสธ",
+    "DISABLE" => "ปิดการใช้งาน",
+    "DISABLED" => "ปิดการใช้งานอยู่",
+    "EDIT" => "แก้ไข",
+    "ENABLE" => "เปิด",
+    "ENABLED" => "เปิดอยู่",
+    "OVERWRITE" => "เขียนทับ",
+    "RESET" => "รีเซ็ต",
+    "SAVE" => "บันทึก",
+    "SEARCH" => "ค้นหา",
+    "SORT" => "ประเภท",
+    "SUBMIT" => "ส่ง",
+    "PRINT" => "พิมพ์",
+    "REMOVE" => "เอาออก",
+    "UNACTIVATED" => "ไม่มีการเปิดใช้",
+    "UPDATE" => "ปรับปรุง",
+    "YES" => "ใช่",
+    "NO" => "ไม่",
+    "OPTIONAL" => "ตัวเลือกเพิ่มเติม",
+
+    // Misc.
+    "BUILT_WITH_UF" => "สร้างด้วย <a href=\"http://www.userfrosting.com\">UserFrosting</a>",
+    "ADMINLTE_THEME_BY" => "ธีมโดย <strong><a href=\"http://almsaeedstudio.com\">Almsaeed Studio</a></strong> สงวนลิขสิทธิ์"
+];
---
+++ app/sprinkles/core/locale/th_TH/validate.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * th_TH
+ *
+ * Thai generic message translations for the core sprinkle.
+ *
+ * @package UserFrosting
+ * @link http://www.userfrosting.com/components/#i18n
+ * @author Karuhut Komol
+ */
+
+return [
+	"VALIDATE" => [
+        "ARRAY" => "ค่าของ '{{label}}' จะต้องเป็น Array",
+        "BOOLEAN" => "ค่าของ '{{label}}' จะต้องเป็น '0' หรือ '1'",
+        "INTEGER" => "ค่าของ '{{label}}' จะต้องเป็นตัวเลข",
+        "INVALID_EMAIL" => "ที่อยู่อีเมลไม่ถูกต้อง",
+        "LENGTH_RANGE" => "ความยาวของ {{label}} จะต้องอยู่ระหว่าง {{min}} ถึง {{max}} ตัวอักษร",
+        "NO_LEAD_WS" => "ค่าของ '{{label}}' ไม่สามารถเริ่มต้นด้วยช่องว่าง หรือ แท็บ",
+        "NO_TRAIL_WS" => "ค่าของ  '{{label}}' ไม่สามารถลงท้ายด้วยช่องว่าง หรือ แท็บ",
+        "REQUIRED" => "กรุณากำหนดค่าของ '{{label}}'"
+    ]
+];
