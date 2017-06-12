b5cdb56c730fed48aec168fa8dd9768dd4da2afa
Louis Charette <charette.louis@gmail.com>
2017-06-12 19:36:52 -0400

    [Bakery] Clear cache command

---
+++ app/system/Bakery/ClearCacheCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2013-2016 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
+ */
+namespace UserFrosting\System\Bakery;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use UserFrosting\System\Bakery\Bakery;
+
+/**
+ * Debug CLI Tools.
+ * Perform the preflight check for UserFrosting install
+ *
+ * @extends Bakery
+ * @author Alex Weissman (https://alexanderweissman.com)
+ */
+class ClearCacheCommand extends Bakery
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName("clear-cache")
+             ->setDescription("Clears the application cache");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->io->title("Clearing cache");
+        $this->ci->cache->flush();
+        $this->io->success("Done !");
+    }
+}
