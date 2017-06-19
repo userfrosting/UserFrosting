ddccfd50261c97c95d350c9c11fdaaf7d882dc3c
alexweissman <alex@alexanderweissman.com>
2017-06-18 21:50:01 -0400

    oops, forgot the -alpha

--- app/defines.php
+++ app/defines.php
@@ -3,7 +3,7 @@
 namespace UserFrosting;
 
 // Some standard defines
-define('UserFrosting\VERSION', '4.1.0');
+define('UserFrosting\VERSION', '4.1.0-alpha');
 define('UserFrosting\DS', '/');
 define('UserFrosting\PHP_MIN_VERSION', '5.6');
 define('UserFrosting\DEBUG_CONFIG', false);
