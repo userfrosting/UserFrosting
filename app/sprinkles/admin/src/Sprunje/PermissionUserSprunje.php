f6a8029cace43c544146079b273d0f7437830f75
alexweissman <alex@alexanderweissman.com>
2017-07-27 13:11:17 -0400

    Simplify PermissionUserSprunje by extending UserSprunje

--- app/sprinkles/admin/src/Sprunje/PermissionUserSprunje.php
+++ app/sprinkles/admin/src/Sprunje/PermissionUserSprunje.php
@@ -3,14 +3,12 @@
  * UserFrosting (http://www.userfrosting.com)
  *
  * @link      https://github.com/userfrosting/UserFrosting
- * @copyright Copyright (c) 2013-2017 Alexander Weissman
  * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
  */
 namespace UserFrosting\Sprinkle\Admin\Sprunje;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use UserFrosting\Sprinkle\Core\Facades\Debug;
-use UserFrosting\Sprinkle\Core\Sprunje\Sprunje;
 use UserFrosting\Support\Exception\BadRequestException;
 
 /**
@@ -20,24 +18,9 @@ use UserFrosting\Support\Exception\BadRequestException;
  *
  * @author Alex Weissman (https://alexanderweissman.com)
  */
-class PermissionUserSprunje extends Sprunje
+class PermissionUserSprunje extends UserSprunje
 {
     protected $name = 'permission_users';
-
-    protected $sortable = [
-        'name',
-        'flag_enabled',
-        'status'
-    ];
-
-    protected $filterable = [
-        'name',
-        'flag_enabled'
-    ];
-
-    protected $excludeForAll = [
-        'flag_enabled'
-    ];
 
     /**
      * {@inheritDoc}
@@ -60,94 +43,5 @@ class PermissionUserSprunje extends Sprunje
         $query = $permission->users()->withVia('roles_via');
 
         return $query;
-    }
-
-    /**
-     * Apply pagination based on the `page` and `size` options.
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    protected function applyPagination()
-    {
-        if (
-            ($this->options['page'] !== null) &&
-            ($this->options['size'] !== null) &&
-            ($this->options['size'] != 'all')
-        ) {
-            $offset = $this->options['size']*$this->options['page'];
-            $this->query = $this->query
-                            ->withLimit($this->options['size'])->withOffset($offset);
-        }
-
-        return $this->query;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function applyTransformations($collection)
-    {
-        // Exclude password field from results
-        $collection->transform(function ($item, $key) {
-            unset($item['password']);
-            return $item;
-        });
-
-        return $collection;
-    }
-
-    /**
-     * Get the unpaginated count of items (after filtering) in this query.
-     *
-     * @return int
-     */
-    protected function countFiltered()
-    {
-        return $this->count();
-    }
-
-    /**
-     * Filter LIKE the first name, last name, or email.
-     *
-     * @param Builder $query
-     * @param mixed $value
-     * @return Builder
-     */
-    protected function filterName($query, $value)
-    {
-        // Split value on separator for OR queries
-        $values = explode($this->orSeparator, $value);
-        return $query->where(function ($query) use ($values) {
-            foreach ($values as $value) {
-                $query = $query->orLike('first_name', $value)
-                                ->orLike('last_name', $value)
-                                ->orLike('email', $value);
-            }
-            return $query;
-        });
-    }
-
-    /**
-     * Sort based on last name.
-     *
-     * @param Builder $query
-     * @param string $direction
-     * @return Builder
-     */
-    protected function sortName($query, $direction)
-    {
-        return $query->orderBy('last_name', $direction);
-    }
-
-    /**
-     * Sort active, unactivated, disabled
-     *
-     * @param Builder $query
-     * @param string $direction
-     * @return Builder
-     */
-    protected function sortStatus($query, $direction)
-    {
-        return $query->orderBy('flag_enabled', $direction)->orderBy('flag_verified', $direction);
     }
 }
