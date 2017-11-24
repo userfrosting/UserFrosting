26c267fc4dc257be87b2d2fc591206dc73071902
Amin Akbari <aminakbari@live.com>
2017-11-24 15:21:44 +0100

    Added Farsi/Persian language to the sprinkles core, account and admin. Plus the default configuration file in core

---
+++ app/sprinkles/account/locale/fa/validate.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
+ *
+ * Standard Farsi/Persian message token translations for the 'account' sprinkle.
+ *
+ * @package userfrosting\i18n\fa
+ * @author aminakbari
+ */
+
+return [
+	"VALIDATE" => [
+        "PASSWORD_MISMATCH" => "گذرواژه و تکرار آن باید با یکدیگر تطبیق پیدا کنند",
+        "USERNAME" => "نام کاربری فقط میتواند از حروف کوچک، اعداد، '.'، '-' و '_' متشکل شوند."
+    ]
+];
