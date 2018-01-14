5b25fc824377f41e865b37d8e08108058b5324fb
Louis Charette <charette.louis@gmail.com>
2018-01-13 21:55:06 -0500

    Fix issue with CacheAlertStream

--- app/sprinkles/core/src/ServicesProvider/ServicesProvider.php
+++ app/sprinkles/core/src/ServicesProvider/ServicesProvider.php
@@ -149,7 +149,7 @@ class ServicesProvider
         /**
          * Cache service.
          *
-         * @return \Illuminate\Cache\TaggedCache
+         * @return \Illuminate\Cache\Repository
          */
         $container['cache'] = function ($c) {
 
