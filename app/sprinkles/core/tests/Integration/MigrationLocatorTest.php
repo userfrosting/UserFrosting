6633b1bfa79c98a05039c89aca2c3b4c07152b24
Louis Charette <charette.louis@gmail.com>
2018-05-21 10:57:56 -0400

    Fix testGetMigrationsWithNoMigrationDir Test

--- app/sprinkles/core/tests/Integration/MigrationLocatorTest.php
+++ app/sprinkles/core/tests/Integration/MigrationLocatorTest.php
@@ -23,6 +23,36 @@ class MigrationLocatorTest extends TestCase
     }
 
     /**
+     * Make sure no error is thrown if the Migration dir doesn't exist
+     */
+    public function testGetMigrationsWithNoMigrationDir()
+    {
+        // Setup mock locator
+        $resourceLocator = m::mock(ResourceLocator::class);
+
+        // Setup mock stream
+        $resourceStream = m::mock(ResourceStream::class);
+        $resourceStream->shouldReceive('getPath')->andReturn('src/Database/Migrations');
+
+        // Setup mock locations
+        $resourceCoreLocation = m::mock(ResourceLocation::class);
+        $resourceCoreLocation->shouldReceive('getName')->andReturn('Core');
+        $resourceCoreLocation->shouldReceive('getPath')->andReturn('app/sprinkles/Core');
+        $resourceAccountLocation = m::mock(ResourceLocation::class);
+        $resourceAccountLocation->shouldReceive('getName')->andReturn('account');
+        $resourceAccountLocation->shouldReceive('getPath')->andReturn('app/sprinkles/Account');
+
+        // When `MigrationLocator` will ask the resource locator to `listResources`, we simulate returning no Resources
+        $resourceLocator->shouldReceive('listResources')->once()->andReturn([]);
+        $locator = new MigrationLocator($resourceLocator);
+        $results = $locator->getMigrations();
+
+        // Test results match expectations
+        $this->assertCount(0, $results);
+        $this->assertEquals([], $results);
+    }
+
+    /**
      * Make sure migrations can be returned for all sprinkles
      */
     public function testGetMigrations()
@@ -54,12 +84,11 @@ class MigrationLocatorTest extends TestCase
             new Resource($resourceStream, $resourceAccountLocation, '', 'CreateMainTable.php')
         ]);
 
-        // Create a new MigrationLocator instance with our simulated SprinkleManager and filesystem
-        // and ask to find core sprinkle migration files
+        // Create a new MigrationLocator instance with our simulated ResourceLocation
         $locator = new MigrationLocator($resourceLocator);
         $results = $locator->getMigrations();
 
-        // The `getMigrationForSprinkle` method should return this
+        // The `getMigration` method should return this
         $expected = [
             '\\UserFrosting\\Sprinkle\\Core\\Database\\Migrations\\one\\CreateUsersTable',
             '\\UserFrosting\\Sprinkle\\Core\\Database\\Migrations\\one\\CreatePasswordResetsTable',
@@ -76,19 +105,6 @@ class MigrationLocatorTest extends TestCase
         $this->assertEquals($expected, $results);
 
         return $locator;
-    }
-
-    /**
-     * Make sure no error is thrown if the Migration dir doesn't exist
-     *
-     * @param  MigrationLocator $locator
-     * @depends testGetMigrations
-     */
-    public function testGetMigrationsWithNoMigrationDir(MigrationLocator $locator)
-    {
-        /*$results = $locator->getMigrations();
-        $this->assertCount(0, $results);
-        $this->assertEquals([], $results);*/
     }
 
     /**
