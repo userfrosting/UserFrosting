4df87a22c8353b4197160c05232fb0c658fbd108
Louis Charette <charette.louis@gmail.com>
2018-05-23 20:53:14 -0400

    Added the `migration` option to the migrate:rollback Bakery command (Ref #730)

--- app/sprinkles/core/src/Bakery/MigrateRollbackCommand.php
+++ app/sprinkles/core/src/Bakery/MigrateRollbackCommand.php
@@ -31,6 +31,7 @@ class MigrateRollbackCommand extends MigrateCommand
              ->addOption('pretend', 'p', InputOption::VALUE_NONE, 'Run migrations in "dry run" mode.')
              ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force the operation to run when in production.')
              ->addOption('database', 'd', InputOption::VALUE_REQUIRED, 'The database connection to use.')
+             ->addOption('migration', 'm', InputOption::VALUE_REQUIRED, 'The specific migration to rollback.')
              ->addOption('steps', 's', InputOption::VALUE_REQUIRED, 'Number of batch to rollback.', 1);
     }
 
@@ -44,13 +45,19 @@ class MigrateRollbackCommand extends MigrateCommand
         // Get options
         $steps = $input->getOption('steps');
         $pretend = $input->getOption('pretend');
+        $migration = $input->getOption('migration');
 
         // Get migrator
         $migrator = $this->setupMigrator($input);
 
         // Rollback migrations
         try {
-            $migrated = $migrator->rollback(['pretend' => $pretend, 'steps' => $steps]);
+            // If we have a specific to rollback, do this. Otherwise, do a normal rollback
+            if ($migration) {
+                $migrated = $migrator->rollbackMigration($migration, ['pretend' => $pretend]);
+            } else {
+                $migrated = $migrator->rollback(['pretend' => $pretend, 'steps' => $steps]);
+            }
         } catch (\Exception $e) {
             $this->io->writeln($migrator->getNotes());
             $this->io->error($e->getMessage());
