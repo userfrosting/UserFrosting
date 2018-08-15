a96bcb0d67b013c576000897c044f77fd8875f5a
Louis Charette <charette.louis@gmail.com>
2018-08-14 20:48:08 -0400

    Prepare for alpha 1 release

--- app/defines.php
+++ app/defines.php
@@ -3,7 +3,7 @@
 namespace UserFrosting;
 
 // Some standard defines
-define('UserFrosting\VERSION', '4.2.0-alpha');
+define('UserFrosting\VERSION', '4.2.0-alpha.1');
 define('UserFrosting\DS', '/');
 define('UserFrosting\PHP_MIN_VERSION', '5.6');
 define('UserFrosting\DEBUG_CONFIG', false);
