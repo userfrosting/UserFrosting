dcc8e2a8c6d91136cbf3a33e0bad4ccf489c916c
Louis Charette <charette.louis@gmail.com>
2018-10-31 22:08:02 -0400

    Adding more test for Authenticator

--- app/sprinkles/account/tests/Integration/AuthenticatorTest.php
+++ app/sprinkles/account/tests/Integration/AuthenticatorTest.php
@@ -41,4 +41,90 @@ class AuthenticatorTest extends TestCase
         $this->assertInstanceOf(Authenticator::class, $authenticator);
         return $authenticator;
     }
+
+    /**
+     * @depends testConstructor
+     * @covers Authenticator::login
+     * @covers Authenticator::logout
+     * @param Authenticator $authenticator
+     */
+    public function testLogin(Authenticator $authenticator)
+    {
+        // Create a test user
+        $testUser = $this->createTestUser();
+
+        // Test session to avoid false positive
+        $key = $this->ci->config['session.keys.current_user_id'];
+        $this->assertNull($this->ci->session[$key]);
+        $this->assertNotSame($testUser->id, $this->ci->session[$key]);
+
+        // Login the test user
+        $authenticator->login($testUser, false);
+
+        // Test session to see if user was logged in
+        $this->assertNotNull($this->ci->session[$key]);
+        $this->assertSame($testUser->id, $this->ci->session[$key]);
+
+        // Must logout to avoid test issue
+        $authenticator->logout();
+
+        // We'll test the logout system works too while we're at it (and depend on it)
+        $key = $this->ci->config['session.keys.current_user_id'];
+        $this->assertNull($this->ci->session[$key]);
+        $this->assertNotSame($testUser->id, $this->ci->session[$key]);
+    }
+
+    /**
+     * @depends testConstructor
+     * @expectedException \UserFrosting\Sprinkle\Account\Authenticate\Exception\AccountInvalidException
+     * @param Authenticator $authenticator
+     */
+    public function testValidateUserAccountThrowAccountInvalidException(Authenticator $authenticator)
+    {
+        $this->invokeMethod($authenticator, 'validateUserAccount', [99999999]);
+    }
+
+    /**
+     * @depends testConstructor
+     * @param Authenticator $authenticator
+     */
+    public function testValidateUserAccountRetunNullOnFalseArgument(Authenticator $authenticator)
+    {
+        $user = $this->invokeMethod($authenticator, 'validateUserAccount', [false]);
+        $this->assertNull($user);
+    }
+
+    /**
+     * @depends testConstructor
+     * @expectedException \UserFrosting\Sprinkle\Account\Authenticate\Exception\AccountInvalidException
+     * @param Authenticator $authenticator
+     */
+    public function testValidateUserAccountThrowExceptionArgumentNotInt(Authenticator $authenticator)
+    {
+        $this->invokeMethod($authenticator, 'validateUserAccount', ['stringIsNotInt']);
+    }
+
+    /**
+     * @depends testConstructor
+     * @param Authenticator $authenticator
+     */
+    public function testValidateUserAccount(Authenticator $authenticator)
+    {
+        $testUser = $this->createTestUser();
+        $user = $this->invokeMethod($authenticator, 'validateUserAccount', [$testUser->id]);
+        $this->assertSame($testUser->id, $user->id);
+    }
+
+    /**
+     * @depends testConstructor
+     * @expectedException \UserFrosting\Sprinkle\Account\Authenticate\Exception\AccountDisabledException
+     * @param Authenticator $authenticator
+     */
+    public function testValidateUserAccountWithAccountDisabledException(Authenticator $authenticator)
+    {
+        $testUser = $this->createTestUser();
+        $testUser->flag_enabled = false;
+        $testUser->save();
+        $this->invokeMethod($authenticator, 'validateUserAccount', [$testUser->id]);
+    }
 }
--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -166,6 +166,23 @@ class TestCase extends BaseTestCase
     }
 
     /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
      * Register a callback to be run before the application is destroyed.
      *
      * @param  callable  $callback
