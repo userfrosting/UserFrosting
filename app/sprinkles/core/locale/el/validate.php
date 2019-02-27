8774ca5435acebf2ecf2920e5ab366be3590aaa8
Lena Stergatou <lenasterg@users.noreply.github.com>
2019-02-28 01:52:18 +0200

    Greek locale files for core sprinkle (#940)
    Thanks @lenasterg !

---
+++ app/sprinkles/core/locale/el/validate.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
+ *
+ * Greek message token translations for the 'core' sprinkle.
+ *
+ * @package userfrosting\i18n\el
+ * @author Lena Stergatou
+ */
+return [
+	"VALIDATE" => [
+            "ARRAY" => "Οι τιμές για <strong>{{label}}</strong> πρέπει να βρίσκονται σε πίνακα.",
+            "BOOLEAN" => "Η τιμή του <strong>{{label}}</strong> πρέπει να είναι είτε '0' είτε '1'.",
+            "INTEGER" => "Η τιμή του <strong>{{label}}</strong> πρέπει να είναι ακέραιος.",
+            "INVALID_EMAIL" => "Μη έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.",
+            "LENGTH_RANGE" => "Το {{label}} πρέπει να έχει μήκος από {{min}} έως {{max}} χαρακτήρες.",
+            "MAX_LENGTH" => "Το {{label}} πρέπει να έχει το πολύ {{max}} χαρακτήρες.",
+            "MIN_LENGTH" => "Το {{label}} πρέπει να έχει τουλάχιστον {{min}} χαρακτήρες.",
+            "NO_LEAD_WS" => "Η τιμή του <strong>{{label}}</strong> δεν μπορεί να ξεκινήσει με κενά, στηλοθέτες ή άλλου τύπου κενό διάστημα.",
+            "NO_TRAIL_WS" => "Η τιμή του <strong>{{label}}</strong> δεν μπορεί να τερματίζει σε κενά, τηλοθέτες ή άλλου τύπου κενό διάστημα.",
+            "RANGE" => "Η τιμή του <strong>{{label}}</strong> πρέπει να είναι μεταξύ {{min}} και {{max}.",
+            "REQUIRED" => "Παρακαλούμε προσδιορίστε μια τιμή για το <strong>{{label}}</strong>.",
+            "SPRUNJE" => [
+                "BAD_FILTER" => "Το <strong>{{name}}</strong> δεν είναι έγκυρο φίλτρο για αυτό το Sprunje.",
+                "BAD_LIST" => "Το <strong>{{name}}</strong> δεν είναι έγκυρη λίστα για αυτό το Sprunje.",
+                "BAD_SORT" => "Το <strong>{{name}}</strong> δεν είναι έγκυρο πεδίο ταξινόμησης για αυτό το Sprunje."
+            ]
+        ]
+    ];
