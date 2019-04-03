39a1d5c94fd320da6ea9b540026b7dbc92decaf6
Louis Charette <charette.louis@gmail.com>
2019-04-02 21:13:07 -0400

    Simply this test

--- app/sprinkles/core/tests/Integration/DatabaseMigratorServiceTest.php
+++ app/sprinkles/core/tests/Integration/DatabaseMigratorServiceTest.php
@@ -9,31 +9,18 @@
 
 namespace UserFrosting\Sprinkle\Core\Tests\Integration;
 
-use UserFrosting\Sprinkle\Core\Tests\TestDatabase;
+use UserFrosting\Sprinkle\Core\Database\Migrator\Migrator;
 use UserFrosting\Tests\TestCase;
 
 /**
- *    Tests for the Migrator Service
+ * Tests for the Migrator Service
  *
- *    @author Louis Charette
+ * @author Louis Charette
  */
 class DatabaseMigratorServiceTest extends TestCase
 {
-    use TestDatabase;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Setup test database
-        $this->setupTestDatabase();
-    }
-
     public function testMigratorService()
     {
-        $this->assertInstanceOf('UserFrosting\Sprinkle\Core\Database\Migrator\Migrator', $this->ci->migrator);
+        $this->assertInstanceOf(Migrator::class, $this->ci->migrator);
     }
 }
