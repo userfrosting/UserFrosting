ad603b13ebc03028f7ff2d0a9dba532ded9e9450
Louis Charette <charette.louis@gmail.com>
2019-05-24 22:06:07 -0400

    Continued AccountController tests

--- app/sprinkles/account/tests/Integration/Controller/AccountControllerTest.php
+++ app/sprinkles/account/tests/Integration/Controller/AccountControllerTest.php
@@ -12,8 +12,11 @@ namespace UserFrosting\Sprinkle\Account\Tests\Integration\Controller;
 
 use Mockery as m;
 use UserFrosting\Sprinkle\Account\Controller\AccountController;
+use UserFrosting\Sprinkle\Account\Facades\Password;
 use UserFrosting\Sprinkle\Account\Repository\PasswordResetRepository;
 use UserFrosting\Sprinkle\Account\Tests\withTestUser;
+use UserFrosting\Sprinkle\Core\Mail\Mailer;
+use UserFrosting\Sprinkle\Core\Mail\TwigMailMessage;
 use UserFrosting\Sprinkle\Core\Tests\RefreshDatabase;
 use UserFrosting\Sprinkle\Core\Tests\TestDatabase;
 use UserFrosting\Sprinkle\Core\Tests\withController;
@@ -81,6 +84,7 @@ class AccountControllerTest extends TestCase
         ]);
 
         $result = $controller->checkUsername($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 200);
         $this->assertSame('true', (string) $result->getBody());
     }
@@ -113,6 +117,7 @@ class AccountControllerTest extends TestCase
         ]);
 
         $result = $controller->checkUsername($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 200);
         $this->assertNotSame('', (string) $result->getBody());
         $this->assertNotSame('true', (string) $result->getBody());
@@ -121,11 +126,10 @@ class AccountControllerTest extends TestCase
     /**
      * @depends testControllerConstructor
      * @depends testcheckUsername
-     * @param  AccountController $controller
      */
-    public function testcheckUsernameWithThrottler(AccountController $controller)
+    public function testcheckUsernameWithThrottler()
     {
-        // Create fake mailer
+        // Create fake throttler
         $throttler = m::mock(Throttler::class);
         $throttler->shouldReceive('getDelay')->once()->with('check_username_request')->andReturn(90);
         $this->ci->throttler = $throttler;
@@ -138,6 +142,7 @@ class AccountControllerTest extends TestCase
         ]);
 
         $result = $controller->checkUsername($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 429);
         $this->assertJson((string) $result->getBody());
         $this->assertSame('[]', (string) $result->getBody());
@@ -145,11 +150,10 @@ class AccountControllerTest extends TestCase
 
     /**
      * @depends testControllerConstructor
-     * @param  AccountController $controller
      */
-    public function testdenyResetPassword(AccountController $controller)
+    public function testdenyResetPassword()
     {
-        // Create fake mailer
+        // Create fake throttler
         $repoPasswordReset = m::mock(PasswordResetRepository::class);
         $repoPasswordReset->shouldReceive('cancel')->once()->with('potato')->andReturn(true);
         $this->ci->repoPasswordReset = $repoPasswordReset;
@@ -162,6 +166,7 @@ class AccountControllerTest extends TestCase
         ]);
 
         $result = $controller->denyResetPassword($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 302);
         $this->assertEquals($this->ci->router->pathFor('login'), $result->getHeaderLine('Location'));
 
@@ -174,11 +179,10 @@ class AccountControllerTest extends TestCase
     /**
      * @depends testControllerConstructor
      * @depends testdenyResetPassword
-     * @param  AccountController $controller
      */
-    public function testdenyResetPasswordWithFailedPasswordReset(AccountController $controller)
+    public function testdenyResetPasswordWithFailedPasswordReset()
     {
-        // Create fake mailer
+        // Create fake repoPasswordReset
         $repoPasswordReset = m::mock(PasswordResetRepository::class);
         $repoPasswordReset->shouldReceive('cancel')->once()->with('potato')->andReturn(false);
         $this->ci->repoPasswordReset = $repoPasswordReset;
@@ -191,6 +195,7 @@ class AccountControllerTest extends TestCase
         ]);
 
         $result = $controller->denyResetPassword($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 302);
         $this->assertEquals($this->ci->router->pathFor('login'), $result->getHeaderLine('Location'));
 
@@ -208,6 +213,7 @@ class AccountControllerTest extends TestCase
     public function testdenyResetPasswordWithFailedValidation(AccountController $controller)
     {
         $result = $controller->denyResetPassword($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
         $this->assertSame($result->getStatusCode(), 302);
         $this->assertEquals($this->ci->router->pathFor('login'), $result->getHeaderLine('Location'));
 
@@ -215,6 +221,319 @@ class AccountControllerTest extends TestCase
         $ms = $this->ci->alerts;
         $messages = $ms->getAndClearMessages();
         $this->assertSame('danger', end($messages)['type']);
+    }
+
+    /**
+     * N.B.: This test is incomplete as it doesn't actually check if
+     *       repoPasswordReset returns the correct info and the message contains
+     *       the right content
+     * @depends testControllerConstructor
+     */
+    public function testforgotPassword()
+    {
+        // Create fake mailer
+        $mailer = m::mock(Mailer::class);
+        $mailer->shouldReceive('send')->once()->with(\Mockery::type(TwigMailMessage::class));
+        $this->ci->mailer = $mailer;
+
+        // Create fake user to test
+        $user = $this->createTestUser(false, false, [
+            'email' => 'foo@bar.com'
+        ]);
+
+        // Recreate controller to use fake mailer
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'email' => 'foo@bar.com'
+        ]);
+
+        $result = $controller->forgotPassword($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 200);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('success', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @depends testforgotPassword
+     * @param  AccountController $controller
+     */
+    public function testforgotPasswordWithFailedValidation(AccountController $controller)
+    {
+        $request = $this->getRequest()->withParsedBody([]);
+
+        $result = $controller->forgotPassword($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 400);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('danger', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @depends testforgotPassword
+     */
+    public function testforgotPasswordWithThrottler()
+    {
+        // Create fake throttler
+        $throttler = m::mock(Throttler::class);
+        $throttler->shouldReceive('getDelay')->once()->with('password_reset_request', ['email' => 'foo@bar.com'])->andReturn(90);
+        $this->ci->throttler = $throttler;
+
+        // Recreate controller to use fake throttler
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'email' => 'foo@bar.com'
+        ]);
+
+        $result = $controller->forgotPassword($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 429);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testgetModalAccountTos(AccountController $controller)
+    {
+        $result = $controller->getModalAccountTos($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 200);
+        $this->assertNotSame('', (string) $result->getBody());
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testimageCaptcha(AccountController $controller)
+    {
+        $result = $controller->imageCaptcha($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 200);
+        $this->assertNotSame('', (string) $result->getBody());
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testlogin(AccountController $controller)
+    {
+        // Create a test user
+        $testUser = $this->createTestUser();
+
+        // Faker doesn't hash the password. Let's do that now
+        $unhashed = $testUser->password;
+        $testUser->password = Password::hash($testUser->password);
+        $testUser->save();
+
+        // Recreate controller to use test user
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'user_name'  => $testUser->user_name,
+            'password'   => $unhashed,
+            'rememberme' => false,
+        ]);
+
+        $result = $controller->login($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        // Can't assert the status code or data, as this can be overwrited by sprinkles
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('success', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @depends testlogin
+     * @param  AccountController $controller
+     */
+    public function testloginWithEmail(AccountController $controller)
+    {
+        // Create a test user
+        $testUser = $this->createTestUser();
+
+        // Faker doesn't hash the password. Let's do that now
+        $unhashed = $testUser->password;
+        $testUser->password = Password::hash($testUser->password);
+        $testUser->save();
+
+        // Recreate controller to use test user
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'user_name'  => $testUser->email,
+            'password'   => $unhashed,
+            'rememberme' => false,
+        ]);
+
+        $result = $controller->login($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        // Can't assert the status code or data, as this can be overwrited by sprinkles
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('success', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testloginWithLoggedInUser(AccountController $controller)
+    {
+        // Create a test user
+        $testUser = $this->createTestUser(false, true);
+
+        // Recreate controller to use fake throttler
+        $controller = $this->getController();
+
+        $result = $controller->login($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 200);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('warning', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testloginWithFailledValidation(AccountController $controller)
+    {
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([]);
+
+        $result = $controller->login($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 400);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('danger', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     */
+    public function testloginWithThrottler()
+    {
+        // Create fake throttler
+        $throttler = m::mock(Throttler::class);
+        $throttler->shouldReceive('getDelay')->once()->with('sign_in_attempt', ['user_identifier' => 'foo'])->andReturn(90);
+        $this->ci->throttler = $throttler;
+
+        // Recreate controller to use fake throttler
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'user_name'  => 'foo',
+            'password'   => 'bar',
+            'rememberme' => false,
+        ]);
+
+        $result = $controller->login($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 429);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('danger', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     */
+    public function testloginWithDisableEmail()
+    {
+        // Force config
+        $this->ci->config['site.login.enable_email'] = false;
+
+        // Recreate controller to use new config
+        $controller = $this->getController();
+
+        // Set POST
+        $request = $this->getRequest()->withParsedBody([
+            'user_name'  => 'foo@bar.com',
+            'password'   => 'bar',
+        ]);
+
+        $result = $controller->login($request, $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 403);
+        $this->assertJson((string) $result->getBody());
+        $this->assertSame('[]', (string) $result->getBody());
+
+        // Test message
+        $ms = $this->ci->alerts;
+        $messages = $ms->getAndClearMessages();
+        $this->assertSame('danger', end($messages)['type']);
+    }
+
+    /**
+     * @depends testControllerConstructor
+     */
+    public function testlogoutWithLoggedInUser()
+    {
+        // Create a test user
+        $testUser = $this->createTestUser(false, true);
+
+        // Recreate controller to use fake throttler
+        $controller = $this->getController();
+
+        $result = $controller->logout($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 302);
+        $this->assertEquals($this->ci->config['site.uri.public'], $result->getHeaderLine('Location'));
+    }
+
+    /**
+     * @depends testControllerConstructor
+     * @param  AccountController $controller
+     */
+    public function testlogoutWithNoUser(AccountController $controller)
+    {
+        $result = $controller->logout($this->getRequest(), $this->getResponse(), []);
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $result);
+        $this->assertSame($result->getStatusCode(), 302);
+        $this->assertEquals($this->ci->config['site.uri.public'], $result->getHeaderLine('Location'));
     }
 
     /**
