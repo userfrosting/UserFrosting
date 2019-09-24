9fb69f59f127d0ad1a3304d47c5c1e0f18959753
Amos Folz <33728190+amosfolz@users.noreply.github.com>
2019-09-24 08:59:15 -0400

    Create serbian locale files to be translated

---
+++ app/sprinkles/account/locale/sr_RS/validate.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
+/**
+ * Serbian message token translations for the 'account' sprinkle.
+ *
+ * @author
+ */
+return [
+    'VALIDATE' => [
+        'PASSWORD_MISMATCH' => 'Your password and confirmation password must match.',
+        'USERNAME'          => "Username may consist only of lowercase letters, numbers, '.', '-', and '_'.",
+    ],
+];
---
+++ app/sprinkles/core/locale/sr_RS/errors.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
+/**
+ * Serbian message token translations for the 'core' sprinkle.
+ *
+ * @author
+ */
+
+return [
+    'ERROR' => [
+        '@TRANSLATION' => 'Error',
+
+        '400' => [
+            'TITLE'       => 'Error 400: Bad Request',
+            'DESCRIPTION' => "It's probably not your fault.",
+        ],
+
+        '404' => [
+            'TITLE'       => 'Error 404: Not Found',
+            'DESCRIPTION' => "We can't seem to find what you're looking for.",
+            'DETAIL'      => 'We tried to find your page...',
+            'EXPLAIN'     => 'We could not find the page you were looking for.',
+            'RETURN'      => 'Either way, click <a href="{{url}}">here</a> to return to the front page.',
+        ],
+
+        'CONFIG' => [
+            'TITLE'       => 'UserFrosting Configuration Issue!',
+            'DESCRIPTION' => 'Some UserFrosting configuration requirements have not been met.',
+            'DETAIL'      => "Something's not right here.",
+            'RETURN'      => 'Please fix the following errors, then <a href="{{url}}">reload</a>.',
+        ],
+
+        'DESCRIPTION' => "We've sensed a great disturbance in the Force.",
+        'DETAIL'      => "Here's what we got:",
+
+        'ENCOUNTERED' => "Uhhh...something happened.  We don't know what.",
+
+        'MAIL' => 'Fatal error attempting mail, contact your server administrator.  If you are the admin, please check the UserFrosting log.',
+
+        'RETURN' => 'Click <a href="{{url}}">here</a> to return to the front page.',
+
+        'SERVER' => "Oops, looks like our server might have goofed. If you're an admin, please check the PHP or UserFrosting logs.",
+
+        'TITLE' => 'Disturbance in the Force',
+    ],
+];
