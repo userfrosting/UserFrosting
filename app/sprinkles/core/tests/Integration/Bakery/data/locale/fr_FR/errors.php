899504d913ef6ea7b7c621dd301cf284f22a70bb
Louis Charette <charette.louis@gmail.com>
2020-01-12 17:49:47 -0500

    Added header

--- app/sprinkles/core/tests/Integration/Bakery/data/locale/fr_FR/errors.php
+++ app/sprinkles/core/tests/Integration/Bakery/data/locale/fr_FR/errors.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
 return [
     'ERROR' => [
         '@TRANSLATION' => 'Erreur',
