b957deaf2e5b9de0031fc0b23e89d0e74705b340
Louis Charette <charette.louis@gmail.com>
2021-04-13 19:50:34 -0400

    Up version

--- app/defines.php
+++ app/defines.php
@@ -11,7 +11,7 @@
 namespace UserFrosting;
 
 // Some standard defines
-define('UserFrosting\VERSION', '4.5.0-dev');
+define('UserFrosting\VERSION', '4.5.0');
 define('UserFrosting\DS', '/');
 define('UserFrosting\PHP_MIN_VERSION', '^7.2');
 define('UserFrosting\PHP_RECOMMENDED_VERSION', '^7.4');
