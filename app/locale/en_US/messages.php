923179310b9710c4f3b925ad9cc3f83935a40117
Louis Charette <lcharette@users.noreply.github.com>
2021-12-07 01:13:49 +0000

    Apply fixes from StyleCI

--- app/locale/en_US/messages.php
+++ app/locale/en_US/messages.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * UserFrosting Core Sprinkle (http://www.userfrosting.com)
+ * UserFrosting (http://www.userfrosting.com)
  *
- * @link      https://github.com/userfrosting/sprinkle-core
- * @copyright Copyright (c) 2021 Alexander Weissman & Louis Charette
- * @license   https://github.com/userfrosting/sprinkle-core/blob/master/LICENSE.md (MIT License)
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
  */
 
 /**
