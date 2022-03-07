8b6b440ac82b2d3eaf8f0fb9cba9559b27d170d4
Louis Charette <charette.louis@gmail.com>
2022-03-06 20:03:57 -0500

    Replace placeholder images

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
 // First off, we'll grab the Composer dependencies
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Workaround to get php built-in server to access assets
+// Workaround to get php built-in server to access legacy assets
 // @see : https://github.com/slimphp/Slim/issues/359#issuecomment-363076423
 if (PHP_SAPI == 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
