f69fd87bbc2535a027a794bea524651dcce3d8fa
Louis Charette <charette.louis@gmail.com>
2023-09-14 21:37:03 -0400

    Use webpack entries files to load dependent sprinkles entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 const Encore = require('@symfony/webpack-encore');
 
+// List dependent sprinkles and local entries files
+const sprinkles = {
+    AdminLTE: require('theme-adminlte/webpack.entries'),
+    Admin: require('sprinkle-admin/webpack.entries'),
+    App: require('./webpack.entries')
+}
+
+// Merge dependent Sprinkles entries with local entries
+let entries = {}
+Object.values(sprinkles).forEach(sprinkle => {
+    entries = Object.assign(entries, sprinkle);
+});
+
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
@@ -9,41 +22,15 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/assets')
+    
     // public path used by the web server to access the output path
     .setPublicPath('/assets/')
+    
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry('app', './app/assets/app.js')
-    //.addEntry('mypage', './assets/mypage.js')
-
-    // ACCOUNT, ADMIN & ADMIN-LTE entries
-    .addEntry('dashboard', './node_modules/sprinkle-admin/app/assets/dashboard.js')
-    .addEntry('page.dashboard', './node_modules/sprinkle-admin/app/assets/page.dashboard.js')
-    .addEntry('page.activities', './node_modules/sprinkle-admin/app/assets/page.activities.js')
-    .addEntry('page.group', './node_modules/sprinkle-admin/app/assets/page.group.js')
-    .addEntry('page.groups', './node_modules/sprinkle-admin/app/assets/page.groups.js')
-    .addEntry('page.role', './node_modules/sprinkle-admin/app/assets/page.role.js')
-    .addEntry('page.roles', './node_modules/sprinkle-admin/app/assets/page.roles.js')
-    .addEntry('page.permission', './node_modules/sprinkle-admin/app/assets/page.permission.js')
-    .addEntry('page.permissions', './node_modules/sprinkle-admin/app/assets/page.permissions.js')
-    .addEntry('page.user', './node_modules/sprinkle-admin/app/assets/page.user.js')
-    .addEntry('page.users', './node_modules/sprinkle-admin/app/assets/page.users.js')
-    .addEntry('page.register', './node_modules/theme-adminlte/app/assets/register.js')
-    .addEntry('page.sign-in', './node_modules/theme-adminlte/app/assets/sign-in.js')
-    .addEntry('page.forgot-password', './node_modules/theme-adminlte/app/assets/forgot-password.js')
-    .addEntry('page.resend-verification', './node_modules/theme-adminlte/app/assets/resend-verification.js')
-    .addEntry('page.set-or-reset-password', './node_modules/theme-adminlte/app/assets/set-or-reset-password.js')
-    .addEntry('page.account-settings', './node_modules/theme-adminlte/app/assets/account-settings.js')
+    // Include all entries
+    .addEntries(entries)
 
     // Copy Favicons
     .copyFiles({ from: './app/assets/favicons', to: 'favicons/[path][name].[hash:8].[ext]' })
@@ -94,7 +81,6 @@ Encore
 
     // uncomment if you use API Platform Admin (composer require api-admin)
     //.enableReactPreset()
-    //.addEntry('admin', './assets/admin.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
---
+++ webpack.entries.js
@@ -0,0 +1,13 @@
+/*
+ * ENTRY CONFIG
+ *
+ * Add 1 entry for each "page" of your app
+ * (including one that's included on every page - e.g. "app")
+ *
+ * Each entry will result in one JavaScript file (e.g. app.js)
+ * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+ */
+
+module.exports = {
+    app: "./app/assets/app.js",
+};
