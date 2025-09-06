3db72d57f462c060353844cde3e9d13fd7cf4530
Louis Charette <charette.louis@gmail.com>
2025-09-06 10:52:41 -0400

    Update Eslint config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Add 'limax' to optimizeDeps (Prevents "importing binding name 'default' cannot be resolved by star export entries")
 - Add YAML loader
 - Update VSCode tasks
+- Update Eslint config
 
 ## [6.0.0-beta.2](https://github.com/userfrosting/UserFrosting/compare/6.0.0-beta.1...6.0.0-beta.2)
 - Update dependencies to version 6.0.0-beta across all packages
---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+        plugins: { js },
+        extends: ['js/recommended']
+    },
+    tseslint.configs.recommended,
+    pluginVue.configs['flat/essential'],
+    { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } }
+])
