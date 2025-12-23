188b82a276fba516eda0e894f85eb52440c4f518
Louis Charette <charette.louis@gmail.com>
2025-12-23 13:17:41 -0500

    Renamed SidebarMenuItems

--- app/assets/layouts/LayoutDashboard.vue
+++ app/assets/layouts/LayoutDashboard.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue'
 import FooterContent from '../components/FooterContent.vue'
-import { SidebarMenuItems } from '@userfrosting/sprinkle-admin/components'
+import { UFAdminSidebarMenuItems } from '@userfrosting/sprinkle-admin/components'
 </script>
 
 <template>
     <NavBar />
     <UFSideBar>
         <UFSideBarLabel :label="$t('ADMIN_PANEL')" />
-        <SidebarMenuItems />
+        <UFAdminSidebarMenuItems />
     </UFSideBar>
     <UFMainContent>
         <UFHeaderPage />
