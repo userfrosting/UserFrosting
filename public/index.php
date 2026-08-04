4160caa42d92463ef92613c4f2fe33825b487955
Louis Charette <charette.louis@gmail.com>
2026-08-03 21:32:51 -0400

    Partially fix Loose comparison issues
    Ref #53

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Workaround to get php built-in server to access legacy assets
 // @see : https://github.com/slimphp/Slim/issues/359#issuecomment-363076423
-if (PHP_SAPI == 'cli-server') {
+if (PHP_SAPI === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 }
 
